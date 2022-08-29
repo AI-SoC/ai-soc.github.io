a22997b073b3d13968a0db7f02d6e676f4dfd5a0
AI-SoC <65325744+AI-SoC@users.noreply.github.com>
2022-08-29 11:17:24 +0900

    Add files via upload

--- symposium/5th/js/db-data.js
+++ symposium/5th/js/db-data.js
@@ -17,13 +17,13 @@ PS = new Array();
 
 
 // Cloud Metaverse
-DB[00] = new Array("1A","color-oral-01","1A","Aug. 29, 2022 (Mon)","09:00 ~ 09:15","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Cloud Metaverse Computing","1A-1"," Cluster with Remote procedure call","<b>Dongkyu Lee, Daejin Park</b> (Kyungpook National University, Korea)<br>","","1A-1.pdf","1B");
+DB[00] = new Array("1A","color-oral-01","1A","Aug. 29, 2022 (Mon)","09:00 ~ 09:30","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Cloud Metaverse Computing","1A-1"," Cluster with Remote procedure call","<b>Dongkyu Lee, Daejin Park</b> (Kyungpook National University, Korea)<br>","","1A-1.pdf","1B");
 
 DB[01] = new Array("","","","","","","","","1A-2","Inverse Reconstruction of Augmented Virtual Reality by Streaming Augmented Landmarks in Edge-Cloud Connected Metaverse Applications","<b>Myungjin Kang, Daejin Park</b> (Kyungpook National University, Korea)<br>","","1A-2.pdf","");
 
 
 // SoC
-DB[02] = new Array("1B","color-oral-02","1B","Aug. 29, 2022 (Mon)","09:30 ~ 09:45","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","System-on-Chip","1B-1","Lightweighted CTS Preconstruction Techniques for Checking Clock Tree Synthesizable Paths in RTL Design Time","<b>Nayoung Kwon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","1B-1.pdf","1C");
+DB[02] = new Array("1B","color-oral-02","1B","Aug. 29, 2022 (Mon)","09:30 ~ 10:15","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","System-on-Chip","1B-1","Lightweighted CTS Preconstruction Techniques for Checking Clock Tree Synthesizable Paths in RTL Design Time","<b>Nayoung Kwon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","1B-1.pdf","1C");
 
 DB[03] = new Array("","","","","","","","","1B-2","RISC-V Basics and Simulation Environments","<b>Seunghyun Park, Daejin Park</b> (Kyungpook National University, Korea)<br>","","1B-2.pdf","");
 
@@ -34,14 +34,14 @@ DB[04] = new Array("","","","","","","","","1B-3","Hardware implementation of lo
 
   
 // Digital Twin
-DB[05] = new Array("1C","color-oral-03","1C","Aug. 29, 2022 (Mon)","09:30 ~ 09:45","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Digital Twin","1C-1","ADC Randomness Compensation Techniques by Enabling Sliding Window-based Partial Inference for Low-Cost ADC","<b>Jisu Kwon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","1C-1.pdf","1D");
+DB[05] = new Array("1C","color-oral-03","1C","Aug. 29, 2022 (Mon)","10:15 ~ 10:45","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Digital Twin","1C-1","ADC Randomness Compensation Techniques by Enabling Sliding Window-based Partial Inference for Low-Cost ADC","<b>Jisu Kwon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","1C-1.pdf","1D");
 
 DB[06] = new Array("","","","","","","","","1C-2","Optimized Replication of ADC-based Particle Counting Algorithm using LUT with Reconfigurable Multi-Variables in Pseudo-Supervised Digital Twining of Reference Dust Sensor Systems","<b>Seungmin Lee, Daejin Park</b> (Kyungpook National University, Korea)<br>","","1C-2.pdf","");
 
 
 // AI-Enabled Algorithm
 
-DB[07] = new Array("1D","color-oral-04","1D","Aug. 29, 2022 (Mon)","09:30 ~ 09:45","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","AI-Enabled Accelerated Software","1D-1","Lightweighted CTS Preconstruction Techniques for Checking Clock Tree Synthesizable Paths in RTL Design Time","<b>Dongkyu Jung, Daejin Park</b> (Kyungpook National University, Korea)<br>","","1D-1.pdf","1E");
+DB[07] = new Array("1D","color-oral-04","1D","Aug. 29, 2022 (Mon)","10:45 ~ 11:30","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","AI-Enabled Accelerated Software","1D-1","Lightweighted CTS Preconstruction Techniques for Checking Clock Tree Synthesizable Paths in RTL Design Time","<b>Dongkyu Jung, Daejin Park</b> (Kyungpook National University, Korea)<br>","","1D-1.pdf","1E");
 
 DB[08] = new Array("","","","","","","","","1D-2","FPGA Realization of Lane Detection Unit using Sliding-based Parallel Segment Detection for Buffer Memory Reduction on TerasIC DE1-SoC","<b>Heuijee Youn, Daejin Park</b> (Kyungpook National University, Korea)<br>","","1D-2.pdf","");
 
@@ -50,13 +50,13 @@ DB[09] = new Array("","","","","","","","","1D-3","Continuous Image-based Fast G
 
 // Compiler Simulation
 
-DB[10] = new Array("1E","color-oral-05","1E","Aug. 29, 2022 (Mon)","09:30 ~ 09:45","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Compiler, Simulator, Emulator","1E-1","Tool Command Languages basics & Simulation Control using Tcl Scripts","<b>Yonghun Lee (Kwonak Analog), Daejin Park</b> (Kyungpook National University, Korea)<br>","","1E-1.pdf","1F");
+DB[10] = new Array("1E","color-oral-05","1E","Aug. 29, 2022 (Mon)","11:30 ~ 12:00","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Compiler, Simulator, Emulator","1E-1","Tool Command Languages basics & Simulation Control using Tcl Scripts","<b>Yonghun Lee (Kwonak Analog), Daejin Park</b> (Kyungpook National University, Korea)<br>","","1E-1.pdf","1F");
 
 DB[11] = new Array("","","","","","","","","1E-2","Optimizing LLVM Transform Passes with Ensemble Reinforcement Learning for Reducing Runtime Memory Consumption","<b>Juneseo Chang (Seoul National University), Daejin Park</b> (Kyungpook National University, Korea)<br>","","1E-2.pdf","");
 
 
 // Digital Signal Processing
-DB[12] = new Array("1F","color-oral-06","1F","Aug. 29, 2022 (Mon)","09:30 ~ 09:45","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Digital Signal Processing","IF-1","Linear Programming Basics","<b>Sungho Cho (LG Display), Daejin Park</b> (Kyungpook National University, Korea)<br>","","1F-1.pdf","1F");
+DB[12] = new Array("1F","color-oral-06","1F","Aug. 29, 2022 (Mon)","12:00 ~ 12:20","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Digital Signal Processing","IF-1","Linear Programming Basics","<b>Sungho Cho (LG Display), Daejin Park</b> (Kyungpook National University, Korea)<br>","","1F-1.pdf","1F");
 
 DB[13] = new Array("","","","","","","","","1F-2","End of Symposium","","","","");
 
