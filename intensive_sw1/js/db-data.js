319852936c770f6b77a1ec75bfc5de9e3c76226a
AI-SoC <65325744+AI-SoC@users.noreply.github.com>
2022-08-30 12:51:11 +0900

    Add files via upload

---
+++ intensive_sw1/js/db-data.js
@@ -0,0 +1,31 @@
+ /*ORDER 
+NAME anchor = SESSION CODE 
+
+		0						 1							2							3			4				5				6				7									8						9					10				11			12				13			
+Session CODE / COLOR CSS / Session Name / Date / Time /  Room / Chair / Session Topic / Paper Code / Paper Title / Author / Belong / PDF / name anchor
+
+SEARCH AREA: Session Topic / Paper Title / Author & Belong  
+*/
+
+/* ARRAY 
+DB		Oral
+PS		Poster
+*/
+
+DB = new Array();
+PS = new Array();
+
+
+// Intro 
+DB[00] = new Array("1A","color-oral-01","1A","Aug. 30, 2022","09:00 ~ 09:50","Room A","Lecturer: Daejin Park (Kyungpook National University, Korea)","Introduction to Embedded System Programming","1A-1","Why/Where/What about Embedded System Programming ?","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","day1_01_intro.pdf","1B");
+
+// Tool
+DB[01] = new Array("1B","color-oral-02","1B","Aug. 30, 2022","10:00 ~ 11:50","Room A","Lecturer: Daejin Park (Kyungpook National University, Korea)","Software Installation","1B-1","Programming Environment Installation (Ubuntu, Visual Studio Code)","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","day1_02_compiler.pdf","1C");
+
+DB[02] = new Array("","","","","","","","","1B-2","Integrating Visual Studio Code on WSL","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","day1_02_vsc_gcc.pdf","");
+
+// Git
+DB[03] = new Array("1C","color-oral-03","1C","Aug. 30, 2022","13:00 ~ 13:50","Room A","Lecturer: Daejin Park (Kyungpook National University, Korea)","Collaborative Compilation","1B-1","Integrateing Projects with Git Repository","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","day1_03_git.pdf","1D");
+
+DB[04] = new Array("","","","","","","","","1B-4","Version Control Concept","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","day1_04_version_control.pdf","");
+
---
+++ intensive_sw1/js/list.js
@@ -0,0 +1,88 @@
+// JavaScript Document
+
+// @ author : Sungmo.Kim
+// @ contact : nemo891124@gmail.com
+// @ date : 2018.11.13
+
+var count=0;
+
+function writeData(list) {
+	var text = '';
+
+		for (var i=0; i<list.length; i++) {
+			var j=i;
+
+			if (list[i][0]!='') {
+				text += 
+					'<div class="section mb-20">' +
+					'  <h5 class="">'+list[i][3]+' / '+list[i][4]+' / '+list[i][5]+'</h5>' +
+					'  <table border="0" cellspacing="2" cellpadding="4" class="tb-session '+list[i][1]+'">' +
+					'    <thead>' +
+					'      <tr>' +
+					'        <th width="120"><div class="code-session">Session '+list[i][2]+' : </div></th>' +
+					'        <th><div class="title-session ">'+list[i][7]+'</div></th>' +
+					'      </tr>' +
+					'    </thead>' +
+					'    <tbody>' +
+					'      <tr>' +
+					'        <td colspan="2"><div class="title-chair pt-10">'+list[i][6]+'</div></td>' +
+					'        </tr>' +
+					'    </tbody>' +
+					'  </table>' +
+					'  <table border="0" cellspacing="2" cellpadding="2" class="tb-paper">' +
+					'    <tbody>' +
+					'      <tr>' +
+					'        <td scope="col" class="th-paper ">No.</td>' +
+					'        <td scope="col" class="th-paper tal pl-50"><span class="ml-50 pl-50">Title / Author(s) </span></td>' +
+					'        <td scope="col" class="th-paper tac video-paper">Video</td>' +
+					'      </tr>';
+			}
+
+			for (j=i; true; j++) {
+
+				text +=
+					'      <tr class="line-paper">' +
+					'        <td valign="top" class="code-paper"><div>'+list[j][8]+'</div></td>' +
+					'        <td class="paper">';
+
+
+					if (list[j][12]!="") {
+						text +=
+							'          <div class="fwb"><a href="'+list[j][12]+'" target="_blank">'+list[j][9]+'</a></div>' +
+							'          <div class="belong "><span class="author">'+list[j][10]+'</span>'+list[j][11]+'</div></td>' +
+							'        <td class="video-paper"><a href="'+list[j][12]+'" target="_blank"><img src="../img/pdf.png" width=25></a></td>' +
+							'      </tr>';
+					} else {
+						text +=
+							'          <div class="fwb">'+list[j][9]+'</div>' +
+							'          <div class="belong "><span class="author">'+list[j][10]+'</span>'+list[j][11]+'</div></td>' +
+							'        <td class="video-paper"></td>' +
+							'      </tr>';
+
+					}
+
+				if (list[j+1]==null||list[j+1][0]!='') break;
+			}
+
+			if (list[i][0]!='') {
+				text +=
+					'    </tbody>' +
+					'  </table>' +
+					'  <a name="'+list[i][13]+'"></a>' +
+					'</div>	' +
+					'<div class="page-nav">' +
+					'  <a href="papers.html" target="_self"><img src="../img/btn_home.gif" align="absmiddle"> HOME </a> &nbsp; ' +
+					'  <a href="javascript:history.back();"><img src="../img/btn_back.gif" align="absmiddle"> BACK </a> &nbsp; ' +
+					'  <a href="#top"><img src="../img/btn_top.gif" align="absmiddle"> TOP </a>' +
+					'</div>';
+			}
+
+			i = j;
+		}
+		
+
+	$("#content-scroll").append(text);
+	return text;
+}
+
+
