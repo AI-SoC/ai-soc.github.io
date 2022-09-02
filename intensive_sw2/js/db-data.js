c7c2216387062e1b52a6c5b38bf008fdee59b861
AI-SoC <65325744+AI-SoC@users.noreply.github.com>
2022-09-02 09:00:21 +0900

    Add files via upload

---
+++ intensive_sw2/js/db-data.js
@@ -0,0 +1,28 @@
+ /*ORDER 
+NAME anchor = SESSION CODE 
+
+		0						 1							2							3			4				5				6				7									8						9					10				11			12				13			
+Session CODE / COLOR CSS / Session Name / Date / Time /  Room / Chair / Session Topic / Paper Code / Paper Title / Author / Belong / PDF / name anchor
+
+SEARCH AREA: Session Topic / Paper Title / Author & Belong  
+*/
+
+/* ARRAY 
+DB		Oral
+PS		Poster
+*/
+
+DB = new Array();
+PS = new Array();
+
+
+// Intro 
+DB[00] = new Array("1A","color-oral-01","1A","Sept. 02, 2022","09:00 ~ 10:50","Room A","Lecturer: Daejin Park (Kyungpook National University, Korea)","Cross Compilation Environment Installation","1A-1","TC275 Infineon Embedded Processors Development Environment","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","00_TC275_Getting_Started.pdf","1B");
+
+DB[01] = new Array("","","","","","","","","1A-2","Infineon Getting Started Manual","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","Infineon-AURIX_Getting_Started_with_AURIX_Development_Studio-GettingStarted-v01_11-EN.pdf","");
+
+DB[02] = new Array("","","","","","","","","1A-3","Infineon Shild Buddy Sensor Acutator Board","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","Infineon-ShieldBuddy_TC275-UM-v02_08-EN.pdf","");
+
+// Tool
+DB[03] = new Array("1B","color-oral-02","1B","Sept. 02, 2022","11:00 ~ 16:50","Room A","Lecturer: Daejin Park (Kyungpook National University, Korea)","Infineon Embedded MCU Programming for GPIO","1B-1","General Purpose I/O Programming","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","01_TC275_GPIO.pdf","1C");
+
