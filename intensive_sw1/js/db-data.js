3a506e48c210a45406bbbd662ae60f7ca12d575b
AI-SoC <65325744+AI-SoC@users.noreply.github.com>
2022-09-03 14:49:46 +0900

    Add files via upload

--- intensive_sw1/js/db-data.js
+++ intensive_sw1/js/db-data.js
@@ -17,50 +17,52 @@ PS = new Array();
 
 
 // Intro 
-DB[00] = new Array("1A","color-oral-01","1A","Aug. 30, 2022","09:00 ~ 09:50","Room A","Lecturer: Daejin Park (Kyungpook National University, Korea)","Introduction to Embedded System Programming","1A-1","Why/Where/What about Embedded System Programming ?","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","day1_01_intro.pdf","1B");
+DB[00] = new Array("1A","color-oral-01","1A","Aug. 30, 2022","09:00 ~ 09:50","Room A","Lecturer: Daejin Park (Kyungpook National University, Korea)","Introduction to Embedded System Programming","1A-1","Why/Where/What about Embedded System Programming ?","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","slide/day1_01_intro.pdf","1A");
 
 // Tool
-DB[01] = new Array("1B","color-oral-02","1B","Aug. 30, 2022","10:00 ~ 11:50","Room A","Lecturer: Daejin Park (Kyungpook National University, Korea)","Software Installation","1B-1","Programming Environment Installation (Ubuntu, Visual Studio Code)","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","day1_02_compiler.pdf","1C");
+DB[01] = new Array("1B","color-oral-02","1B","Aug. 30, 2022","10:00 ~ 11:50","Room A","Lecturer: Daejin Park (Kyungpook National University, Korea)","Software Installation","1B-1","Programming Environment Installation (Ubuntu, Visual Studio Code)","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","slide/day1_02_compiler.pdf","1B");
 
-DB[02] = new Array("","","","","","","","","1B-2","Integrating Visual Studio Code on WSL","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","day1_02_vsc_gcc.pdf","");
-
-// Git
-DB[03] = new Array("1C","color-oral-03","1C","Aug. 30, 2022","13:00 ~ 13:50","Room A","Lecturer: Daejin Park (Kyungpook National University, Korea)","Collaborative Compilation","1C-1","Integrateing Projects with Git Repository","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","day1_03_git.pdf","1D");
-
-DB[04] = new Array("","","","","","","","","1C-2","Version Control Concept","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","day1_04_version_control.pdf","");
+DB[02] = new Array("","","","","","","","","1B-2","Integrating Visual Studio Code on WSL","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","slide/day1_02_vsc_gcc.pdf","");
 
 // Git
-DB[05] = new Array("1D","color-oral-04","1D","Aug. 30, 2022","14:00 ~ 16:50","Room A","Lecturer: Daejin Park (Kyungpook National University, Korea)","Variable Allocation on Memory","1D-1","Building Multiple Files","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","day1_05_multiple_files.pdf","1E");
+DB[03] = new Array("1C","color-oral-03","1C","Aug. 30, 2022","13:00 ~ 13:50","Room A","Lecturer: Daejin Park (Kyungpook National University, Korea)","Collaborative Compilation","1C-1","Integrating Projects with Git Repository","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","slide/day1_03_git.pdf","1C");
 
-DB[06] = new Array("","","","","","","","","1D-2","Variable Liveness Scope","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","day1_06_var_scope.pdf","");
+DB[04] = new Array("","","","","","","","","1C-2","Version Control Concept","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","slide/day1_04_version_control.pdf","");
 
-DB[07] = new Array("","","","","","","","","1D-3","Variable Allocation at Specific Address on Memory","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","day1_07_variable_allocation.pdf","");
+// Git
+DB[05] = new Array("1D","color-oral-04","1D","Aug. 30, 2022","14:00 ~ 16:50","Room A","Lecturer: Daejin Park (Kyungpook National University, Korea)","Variable Allocation on Memory","1D-1","Building Multiple Files","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","slide/day1_05_multiple_files.pdf","1D");
+
+DB[06] = new Array("","","","","","","","","1D-2","Variable Liveness Scope","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","slide/day1_06_var_scope.pdf","");
+
+DB[07] = new Array("","","","","","","","","1D-3","Variable Allocation at Specific Address on Memory","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","slide/day1_07_variable_allocation.pdf","");
 
 
 // Git
-DB[08] = new Array("2A","color-oral-05","2A","Aug. 31, 2022","09:00 ~ 13:50","Room A","Lecturer: Daejin Park (Kyungpook National University, Korea)","Advanced C Programming","2A-1","Traversing Array via Pointer","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","day2_01_array.pdf","2B");
+DB[08] = new Array("2A","color-oral-05","2A","Aug. 31, 2022","09:00 ~ 13:50","Room A","Lecturer: Daejin Park (Kyungpook National University, Korea)","Advanced C Programming","2A-1","Traversing Array via Pointer","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","slide/day2_01_array.pdf","2A");
 
-DB[09] = new Array("","","","","","","","","2A-2","Enumeration-based Constant Abstraction","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","day2_02_enumeration.pdf","");
+DB[09] = new Array("","","","","","","","","2A-2","Enumeration-based Constant Abstraction","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","slide/day2_02_enumeration.pdf","");
 
-DB[10] = new Array("","","","","","","","","2A-3","Pointing Everything by void*","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","day2_03_void_pointer.pdf","");
+DB[10] = new Array("","","","","","","","","2A-3","Pointing Everything by void*","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","slide/day2_03_void_pointer.pdf","");
 
-DB[11] = new Array("","","","","","","","","2A-4","Transforming Program Behavior using Function Pointer","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","day2_04_function_pointer.pdf","");
-
-// Git
-DB[12] = new Array("2B","color-oral-06","2B","Aug. 31, 2022","14:00 ~ 16:50","Room A","Lecturer: Daejin Park (Kyungpook National University, Korea)","Accessing via Memory-Mapped I/O","2B-1","Using volatile for Explicit Memory I/O","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","day2_05_volatile.pdf","2C");
-
-DB[13] = new Array("","","","","","","","","2B-2","Allocating Structured Multi-Bytes Data on Memory using struct","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","day2_06_struct.pdf","");
-
-DB[14] = new Array("","","","","","","","","2B-3","Representing Data on Memory as Various Views using union","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","day2_07_union.pdf","");
+DB[11] = new Array("","","","","","","","","2A-4","Transforming Program Behavior using Function Pointer","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","slide/day2_04_function_pointer.pdf","");
 
 // Git
-DB[15] = new Array("3A","color-oral-07","3A","Sept. 1, 2022","09:00 ~ 11:50","Room A","Lecturer: Daejin Park (Kyungpook National University, Korea)","Code Memory and Data Memory","2B-1","Bit Representation","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","day3_01_bit_representation.pdf","3B");
+DB[12] = new Array("2B","color-oral-06","2B","Aug. 31, 2022","14:00 ~ 16:50","Room A","Lecturer: Daejin Park (Kyungpook National University, Korea)","Accessing via Memory-Mapped I/O","2B-1","Using volatile for Explicit Memory I/O","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","slide/day2_05_volatile.pdf","2B");
 
-DB[16] = new Array("","","","","","","","","2B-2","Layout Instruction on Code Memory, and Allocating Variables on Data Memory","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","day3_02_memory_allocation.pdf","");
+DB[13] = new Array("","","","","","","","","2B-2","Allocating Structured Multi-Bytes Data on Memory using struct","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","slide/day2_06_struct.pdf","");
+
+DB[14] = new Array("","","","","","","","","2B-3","Representing Data on Memory as Various Views using union","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","slide/day2_07_union.pdf","");
 
 // Git
-DB[17] = new Array("3B","color-oral-08","3B","Sept. 1, 2022","13:00 ~ 14:50","Room A","Lecturer: Daejin Park (Kyungpook National University, Korea)","Code Memory and Data Memory","3B-1","Bit Manipulation to Control Hardware","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","day3_03_bit_manipulation.pdf","3C");
+DB[15] = new Array("3A","color-oral-07","3A","Sept. 1, 2022","09:00 ~ 11:50","Room A","Lecturer: Daejin Park (Kyungpook National University, Korea)","Code Memory and Data Memory","2B-1","Bit Representation","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","slide/day3_01_bit_representation.pdf","3A");
 
-DB[18] = new Array("","","","","","","","","3B-2","Hardware Memory Allocation to Access On-Chip Hardware via Memory Mapped I/O","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","day3_04_hardware_allocation.pdf","");
+DB[16] = new Array("","","","","","","","","2B-2","Layout Instruction on Code Memory, and Allocating Variables on Data Memory","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","slide/day3_02_memory_allocation.pdf","");
+
+// Git
+DB[17] = new Array("3B","color-oral-08","3B","Sept. 1, 2022","13:00 ~ 16:50","Room A","Lecturer: Daejin Park (Kyungpook National University, Korea)","Code Memory and Data Memory","3B-1","Bit Manipulation to Control Hardware","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","slide/day3_03_bit_manipulation.pdf","3B");
+
+DB[18] = new Array("","","","","","","","","3B-2","Hardware Memory Allocation to Access On-Chip Hardware via Memory Mapped I/O","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","slide/day3_04_hardware_allocation.pdf","");
+
+DB[19] = new Array("","","","","","","","","3B-3","Bus Architecture-based Polling & Interrupt-Driven Software Execution","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","slide/day3_05_interrupt.pdf","");
 
 
