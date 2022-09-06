49c152138413b2f622e83e8a162899331ed4d201
AI-SoC <65325744+AI-SoC@users.noreply.github.com>
2022-09-06 11:04:12 +0900

    Add files via upload

--- intensive_sw2/js/db-data.js
+++ intensive_sw2/js/db-data.js
@@ -17,29 +17,29 @@ PS = new Array();
 
 
 // Intro 
-DB[00] = new Array("1A","color-oral-01","1A","Sept. 02, 2022","09:00 ~ 10:50","Room A","Lecturer: Daejin Park (Kyungpook National University, Korea)","Cross Compilation Environment Installation","1A-1","TC275 Infineon Embedded Processors Development Environment","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","slide/00_TC275_Getting_Started.pdf","1B");
+DB[00] = new Array("1A","color-oral-01","1A","Sept. 02, 2022","09:00 ~ 10:50","Room A","Lecturer: Daejin Park (Kyungpook National University, Korea)","Cross Compilation Environment Installation","1A-1","TC275 Infineon Embedded Processors Development Environment","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","slide/00_TC275_Getting_Started.pdf","1A");
 
 DB[01] = new Array("","","","","","","","","1A-2","Infineon Getting Started Manual","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","slide/Infineon-AURIX_Getting_Started_with_AURIX_Development_Studio-GettingStarted-v01_11-EN.pdf","");
 
 DB[02] = new Array("","","","","","","","","1A-3","Infineon ShildBuddy Target Embedded Board","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","slide/Infineon-ShieldBuddy_TC275-UM-v02_08-EN.pdf","");
 
 // Tool
-DB[03] = new Array("1B","color-oral-02","1B","Sept. 02, 2022","11:00 ~ 16:50","Room A","Lecturer: Daejin Park (Kyungpook National University, Korea)","Infineon Embedded MCU Programming for GPIO","1B-1","General Purpose I/O Programming","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","slide/01_TC275_GPIO.pdf","1C");
+DB[03] = new Array("1B","color-oral-02","1B","Sept. 02, 2022","11:00 ~ 16:50","Room A","Lecturer: Daejin Park (Kyungpook National University, Korea)","Infineon Embedded MCU Programming for GPIO","1B-1","General Purpose I/O Programming","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","slide/01_TC275_GPIO.pdf","1B");
 
 DB[04] = new Array("","","","","","","","","1B-2","Infineon TC27X D-Step Processors User Manual","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","slide/Infineon-TC27x_D-step-UM-v02_02-EN.pdf","");
 
 DB[05] = new Array("","","","","","","","","1B-2","YWRobot Easy Module Shiled Sensor/Actuator Board","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","slide/ywrobot-easy-module-shield-v1.jpg","");
 
 // Intro
-DB[06] = new Array("2A","color-oral-03","2A","Sept. 05, 2022","09:00 ~ 11:50","Room A","Lecturer: Daejin Park (Kyungpook National University, Korea)","External Interrupt-Driven Asynchronous Hardware-Software Interaction","2A-1","GPIO Interrupt-Driven Software Execution for TC275 Infineon Embedded Processors","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","slide/02_TC275_Interrupt.pdf","2B");
+DB[06] = new Array("2A","color-oral-03","2A","Sept. 05, 2022","09:00 ~ 11:50","Room A","Lecturer: Daejin Park (Kyungpook National University, Korea)","External Interrupt-Driven Asynchronous Hardware-Software Interaction","2A-1","GPIO Interrupt-Driven Software Execution for TC275 Infineon Embedded Processors","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","slide/02_TC275_Interrupt.pdf","2A");
 
 // Intro
-DB[07] = new Array("2B","color-oral-04","2B","Sept. 05, 2022","13:00 ~ 16:50","Room A","Lecturer: Daejin Park (Kyungpook National University, Korea)","Internal Interrupt-Driven Hardware-Software Interaction","2B-1","Timer Interrupt-Driven Periodic Software Execution for TC275 Infineon Embedded Processors","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","slide/03_TC275_Timer.pdf","3A");
+DB[07] = new Array("2B","color-oral-04","2B","Sept. 05, 2022","13:00 ~ 16:50","Room A","Lecturer: Daejin Park (Kyungpook National University, Korea)","Internal Interrupt-Driven Hardware-Software Interaction","2B-1","Timer Interrupt-Driven Periodic Software Execution for TC275 Infineon Embedded Processors","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","slide/03_TC275_Timer.pdf","2B");
 
 // Intro
-DB[08] = new Array("3A","color-oral-05","3A","Sept. 06, 2022","09:00 ~ 16:50","Room A","Lecturer: Daejin Park (Kyungpook National University, Korea)","Sampling and Quantization for Interfacing Analog World","3A-1","ADC-based Voltage Sampling and Quantization for TC275 Infineon Embedded Processors","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","slide/04_TC275_ADC.pdf","4A");
+DB[08] = new Array("3A","color-oral-05","3A","Sept. 06, 2022","09:00 ~ 16:50","Room A","Lecturer: Daejin Park (Kyungpook National University, Korea)","Sampling and Quantization for Interfacing Analog World","3A-1","ADC-based Voltage Sampling and Quantization for TC275 Infineon Embedded Processors","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","slide/04_TC275_ADC.pdf","3A");
 
 // Intro
-DB[09] = new Array("4A","color-oral-05","4A","Sept. 07, 2022","09:00 ~ 16:50","Room A","Lecturer: Daejin Park (Kyungpook National University, Korea)","Driving Analog World using Digital Time-based Modulation","3A-1","PWM-based Energy Tranfer Moudulation for TC275 Infineon Embedded Processors","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","slide/05_TC275_PWM.pdf","5A");
+DB[09] = new Array("4A","color-oral-06","4A","Sept. 07, 2022","09:00 ~ 16:50","Room A","Lecturer: Daejin Park (Kyungpook National University, Korea)","Driving Analog World using Digital Time-based Modulation","4A-1","PWM-based Energy Tranfer Moudulation for TC275 Infineon Embedded Processors","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","slide/05_TC275_PWM.pdf","4A");
 
 
