869cb9f7381768e7f9457ff97f5060fe7a41980e
AI-SoC <65325744+AI-SoC@users.noreply.github.com>
2022-09-08 00:56:12 +0900

    Add files via upload

---
+++ intensive_sw3/js/db-data.js
@@ -0,0 +1,27 @@
+ /*ORDER 
+NAME anchor = SESSION CODE 
+
+		0						 1							2							3			4				5				6				7									8						9					10				11			12				13			
+Session CODE / COLOR CSS / Session Name / Date / Time /  Room / Chair / Session Topic / Paper Code / Paper Title / Author / Belong / PDF / name anchor
+
+SEARCH AREA: Session Topic / Paper Title / Author & Belong  
+*/
+
+/* ARRAY 
+DB		Oral
+PS		Poster
+*/
+
+DB = new Array();
+PS = new Array();
+
+
+// Intro 
+DB[00] = new Array("1A","color-oral-01","1A","Sept. 08, 2022","09:00 ~ 16:50","Room A","Lecturer: Daejin Park (Kyungpook National University, Korea)","Ambient Activity Monitoring","1A-1","Measuring Ambient Activity based on Time of Flight using Time-to-Digital Converion for TC275 Infineon Embedded Processors","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","slide/00_TC275_Ultrasonic.pdf","1A");
+
+
+DB[01] = new Array("2A","color-oral-02","2A","Sept. 14~15, 2022","09:00 ~ 16:50","Room A","Lecturer: Daejin Park (Kyungpook National University, Korea)","Team Project Guide","1A-1","Collaborative Development, Documentation, and Presentation for Successful Projects based on ECU with Embedded On-Chip Software","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","slide/project.pdf","1A");
+
+DB[02] = new Array("","","","","","","","","2A-2","Recommented Sample Projects","<b>Daejin Park</b> (Kyungpook National University, Korea)<br>","","slide/project_examples.xlsx","");
+
+
