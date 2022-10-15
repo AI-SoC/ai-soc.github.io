edf416ee52568583f911e829d02d0654e11bae16
AI-SoC <65325744+AI-SoC@users.noreply.github.com>
2022-10-15 10:40:37 +0900

    Add files via upload

---
+++ symposium/6th/js/db-data.js
@@ -0,0 +1,63 @@
+ /*ORDER 
+NAME anchor = SESSION CODE 
+
+		0						 1							2							3			4				5				6				7									8						9					10				11			12				13			
+Session CODE / COLOR CSS / Session Name / Date / Time /  Room / Chair / Session Topic / Paper Code / Paper Title / Author / Belong / PDF / name anchor
+
+SEARCH AREA: Session Topic / Paper Title / Author & Belong  
+*/
+
+/* ARRAY 
+DB		Oral
+PS		Poster
+*/
+
+DB = new Array();
+PS = new Array();
+
+
+// Cloud Metaverse
+DB[00] = new Array("1A","color-oral-01","1A","Oct. 28, 2022 (Friday)","09:00 ~ 09:30","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Cloud Metaverse Computing","1A-1"," On-demand Streamable Code Executable Link Layer for OpenAPI-based Edge-Cloud Infrastructure","<b>Dongkyu Lee, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/gn3X9mMinn0","1B");
+
+DB[01] = new Array("","","","","","","","","1A-2","Effective IoT Design Exploration Using VR-based Interoperation of Heterogenous-Things Models","<b>Myungjin Kang, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/AbdTvUv0ly8","");
+
+
+// SoC
+DB[02] = new Array("1B","color-oral-02","1B","Oct. 28, 2022 (Friday)","09:30 ~ 10:15","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","System-on-Chip","1B-1","Introduction OpenRoad Project & Future Study","<b>Nayoung Kwon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/R_xmdmKgNmk","1C");
+
+DB[03] = new Array("","","","","","","","","1B-2","Assembly Generation and Optimization with RISC-V RARS Simulator","<b>Seunghyun Park, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/L6Mg77jci9M","");
+
+
+DB[04] = new Array("","","","","","","","","1B-3","Adaptive Active Noise Canceller with Binary Search-based Coefficient Compensation Accelerator for Low-Power Lightweighted Microcontrollers ","<b>Joonghyun Ahn (SK Hynix), Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/DkEpY22tWOw","");
+
+
+  
+// Digital Twin
+DB[05] = new Array("1C","color-oral-03","1C","Oct. 28, 2022 (Friday)","10:15 ~ 10:45","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Digital Twin","1C-1","TinyML: Machine Learning World for Tiny Things","<b>Jisu Kwon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/Y7qq7WChlsQ","1D");
+
+DB[06] = new Array("","","","","","","","","1C-2","Developing MATLAB Apps Using the Model-View-Controller Pattern: Random Data Generation and Statistical Analysis","<b>Seungmin Lee, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/uwWYIetVdUI","");
+
+
+// AI-Enabled Algorithm
+
+DB[07] = new Array("1D","color-oral-04","1D","Oct. 28, 2022 (Friday)","10:45 ~ 11:30","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","AI-Enabled Accelerated Software","1D-1","RTOS : Embedded real-time system implemented as interrupt","<b>Dongkyu Jung, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/MzQWkeTjpdQ","1E");
+
+DB[08] = new Array("","","","","","","","","1D-2","Lane Detection Unit based on Sliding-based Parallel Segment buffer for Lightweighted embedded system","<b>Heuijee Youn, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/UlAwL6L6Wnw","");
+
+DB[09] = new Array("","","","","","","","","1D-3","RTL8211-based Ethernet Schematic and Porting Guide for i.MX6","<b>Sunghoon Hong, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/Tgs2OXOlsVc","");
+
+
+// Compiler Simulation
+
+DB[10] = new Array("1E","color-oral-05","1E","Oct. 28, 2022 (Friday)","11:30 ~ 12:00","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Compiler, Simulator, Emulator","1E-1","SystemVerilog Direct Programming Interface (DPI) Basic and Example","<b>Yonghun Lee (Kwonak Analog), Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/Cj4gKsVgRk4","1F");
+
+DB[11] = new Array("","","","","","","","","1E-2","RLlib: Highly Scalable Reinforcement Learning Framework","<b>Juneseo Chang (Seoul National University), Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/F1n1QmCYxsA","");
+
+
+// Digital Signal Processing
+DB[12] = new Array("1F","color-oral-06","1F","Oct. 28, 2022 (Friday)","12:00 ~ 12:20","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Digital Signal Processing","IF-1","Convolution and Transfer function in LTI system","<b>Sungho Cho (LG Display), Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/wugFo5OD6n4","1F");
+
+DB[13] = new Array("","","","","","","","","1F-2","End of Symposium","","","","");
+
+
+
