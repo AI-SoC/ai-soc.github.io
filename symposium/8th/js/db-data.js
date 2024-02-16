4330e113966bcec9c2133bcd70a509186f15a6b5
AI-SoC <65325744+AI-SoC@users.noreply.github.com>
2024-02-16 19:48:41 +0900

    Add files via upload

---
+++ symposium/8th/js/db-data.js
@@ -0,0 +1,56 @@
+ /*ORDER 
+NAME anchor = SESSION CODE 
+
+		0						 1							2							3			4				5				6				7									8						9					10				11			12				13			
+Session CODE / COLOR CSS / Session Name / Date / Time /  Room / Chair / Session Topic / Paper Code / Paper Title / Author / Belong / PDF / name anchor
+
+SEARCH AREA: Session Topic / Paper Title / Author & Belong  
+*/
+
+/* ARRAY 
+DB		Oral
+PS		Poster
+*/
+
+DB = new Array();
+PS = new Array();
+
+
+// Cloud Metaverse
+DB[00] = new Array("1A","color-oral-01","1A","Dec. 06, 2023 (Friday)","09:00 ~ 09:30","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Cloud Metaverse OS Computing","1A-1","Exploring Scalable and Efficient OS-level Page Management","<b>Juneseo Chang, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/co0egATi4Mc","1B");
+
+DB[01] = new Array("","","","","","","","","1A-2","Vertical Heterogeneous Things' Distributed Computing","<b>Myungjin Kang, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/n4c0UuHeYD8","");
+
+
+// SoC
+DB[02] = new Array("1B","color-oral-02","1B","Dec. 06, 2023 (Friday)","09:30 ~ 10:15","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","System-on-Chip","1B-1","Physical Memory Virtualization on MCU between SRAM & Flash","<b>Jisu Kwon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/m5G0VAI3-I4","1C");
+
+
+DB[03] = new Array("","","","","","","","","1B-2","FFT Implementation on FPGA","<b>Yujeong Son, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/h32iD-7cIW4","");
+
+
+  
+// Digital Twin
+DB[04] = new Array("1C","color-oral-06","1C","Dec. 06, 2023 (Friday)","10:30 ~ 10:45","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Digital Twin-based Digital Signal Processing","IC-1","Iterative Learning for LSTM Initialization in Noise Suppressio","<b>Seungmin Lee, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/thmWEJB4fPs","1D");
+
+
+// AI-Enabled Algorithm
+
+DB[05] = new Array("1D","color-oral-04","1D","Dec. 06, 2023 (Friday)","10:45 ~ 11:30","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","AI Accelerated Embedded Software","1D-1","Weight Table Structure for Lightweight CNN Processing","<b>Dongkyu Jung, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/c-StDor2_mw","1E");
+
+DB[06] = new Array("","","","","","","","","1D-2","Scratch Pad Memory Defragmentation","<b>Gihyun Jeon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/tl-T6s4pFMo","");
+
+DB[07] = new Array("","","","","","","","","1D-3","Runtime Embedded SW Malfunction Detection using I/V Pattern Matching Profile","<b>Sanghoon Lee, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/mODwxF7DpDA","");
+
+
+// Compiler Simulation
+
+DB[08] = new Array("1E","color-oral-05","1E","Dec. 06, 2023 (Friday)","11:30 ~ 12:00","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Hypervisor","1E-1","Hypervisor Implementation of x86","<b>Heejee Yun, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/oIYxbCXNoNk","1F");
+
+DB[09] = new Array("","","","","","","","","1E-2","IO CPU Hypervised Virtualization for Embedded Processors","<b>Minjung Kim, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/pZt2i9shzaI","");
+
+
+DB[10] = new Array("","","","","","","","","1E-2","End of Symposium","","","","");
+
+
+
