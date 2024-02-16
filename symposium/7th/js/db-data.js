3778cac3ee5c96ae2947d5f778362dbea3ee5d46
AI-SoC <65325744+AI-SoC@users.noreply.github.com>
2024-02-16 19:44:11 +0900

    Add files via upload

---
+++ symposium/7th/js/db-data.js
@@ -0,0 +1,59 @@
+ /*ORDER 
+NAME anchor = SESSION CODE 
+
+		0						 1							2							3			4				5				6				7									8						9					10				11			12				13			
+Session CODE / COLOR CSS / Session Name / Date / Time /  Room / Chair / Session Topic / Paper Code / Paper Title / Author / Belong / PDF / name anchor
+
+SEARCH AREA: Session Topic / Paper Title / Author & Belong  
+*/
+
+/* ARRAY 
+DB		Oral
+PS		Poster
+*/
+
+DB = new Array();
+PS = new Array();
+
+
+// Cloud Metaverse
+DB[00] = new Array("1A","color-oral-01","1A","Sept. 25, 2023 (Friday)","09:00 ~ 09:30","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Cloud Metaverse Computing","1A-1","Edge-Cloud Connected Software Execution Virtualization for On-Demand Steamable AI Services","<b>Dongkyu Lee, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/yzuky1hpP84?si=_XhQ_vEcHgpZPXGG","1B");
+
+DB[01] = new Array("","","","","","","","","1A-2","Horizontal & Vertical Heterogenous Things Effective Simulation","<b>Myungjin Kang, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/0z3ARkTQxSY?si=FD6yKpDwqKEO-3-q","");
+
+
+// SoC
+DB[02] = new Array("1B","color-oral-02","1B","Sept. 25, 2023 (Friday)","09:30 ~ 10:15","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","System-on-Chip","1B-1","Efficient Partial Weight Update Techniques for Lightweight On-Device Learning","<b>Jisu Kwon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/uRNwQRcIne4","1C");
+
+
+DB[03] = new Array("","","","","","","","","1B-2","Circuit-level Low-Power SNN Operator using RRAM","<b>Heejee Yun, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/jA4WkXoXZJo","");
+
+
+DB[04] = new Array("","","","","","","","","1B-3","Multi-Instruction Decode Protection on Pipeline Stage using On-Chip Error Correction Accelerator","<b>Yoojeong Son, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/mUi7ZrCyFLc","");
+
+
+  
+// Digital Twin
+DB[05] = new Array("1F","color-oral-06","1F","Sept. 25, 2023 (Friday)","10:30 ~ 10:45","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Digital Twin-based Digital Signal Processing","IC-1","LSTM-based Noise Reduction of SVD Particulate Matter Digital Twining","<b>Seungmin Lee, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/xUBNZl_tdF0","1D");
+
+
+// AI-Enabled Algorithm
+
+DB[06] = new Array("1D","color-oral-04","1D","Sept. 25, 2023 (Friday)","10:45 ~ 11:30","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","AI Accelerated Embedded Software","1D-1","Lightweight CNN Acceleration Function Structure with Data Access Count Optimization","<b>Dongkyu Jung, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/Z0VaKemez5U","1E");
+
+DB[07] = new Array("","","","","","","","","1D-2","Shallow Cache Memory Systems","<b>Gihyun Jeon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/WBBom-ZpilI","");
+
+DB[08] = new Array("","","","","","","","","1D-3","Domain Specific Hardware Accelerator using MLIR IR Level Granularity","<b>Seunghyun Park, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/bR8yu7AA6Fo","");
+
+
+// Compiler Simulation
+
+DB[09] = new Array("1E","color-oral-05","1E","Sept. 25, 2023 (Friday)","11:30 ~ 12:00","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Hypervisor","1E-1","Implementation of Hypervisor","<b>Minjung Kim, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/akoZpQDgx6A","1F");
+
+DB[10] = new Array("","","","","","","","","1E-2","Deep Dive to Virtualization - Hardware Supported Virtualization","<b>Nayoung Kwon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/9B30ZswqUN0","");
+
+
+DB[11] = new Array("","","","","","","","","1E-2","End of Symposium","","","","");
+
+
+
