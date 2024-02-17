5cff325ac84871de3f45916055e1f7b7547aed1c
AI-SoC <65325744+AI-SoC@users.noreply.github.com>
2024-02-17 23:22:28 +0900

    Add files via upload

---
+++ symposium/4th/js/db-data.js
@@ -0,0 +1,57 @@
+ /*ORDER 
+NAME anchor = SESSION CODE 
+
+		0						 1							2							3			4				5				6				7									8						9					10				11			12				13			
+Session CODE / COLOR CSS / Session Name / Date / Time /  Room / Chair / Session Topic / Paper Code / Paper Title / Author / Belong / PDF / name anchor
+
+SEARCH AREA: Session Topic / Paper Title / Author & Belong  
+*/
+
+/* ARRAY 
+DB		Oral
+PS		Poster
+*/
+
+DB = new Array();
+PS = new Array();
+
+
+// Cloud Metaverse
+DB[00] = new Array("1A","color-oral-01","1A","July. 04, 2022 (Friday)","09:00 ~ 09:30","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Cloud Metaverse OS Computing","1A-1","Clustering System: Platforms and Network Connections","<b>Dongkyu Lee, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/DcU8QRd_6pw","1B");
+
+DB[01] = new Array("","","","","","","","","1A-2","Introduction of Distributed Computing","<b>Myungjin Kang, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/2E4p1Rswc-g","");
+
+
+// SoC
+DB[02] = new Array("1B","color-oral-02","1B","July. 04, 2022 (Friday)","09:30 ~ 10:15","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","System-on-Chip","1B-1","Study on the Tensorflow Lite and EdgeTPU Architecture","<b>Jisu Kwon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/oPbZ_ByMTMY","1C");
+
+
+DB[03] = new Array("","","","","","","","","1B-2","FPGA Programming with TerasIC DE1-SoC","<b>Heejee Yun, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/WCo5Gmuyu9o","");
+
+DB[04] = new Array("","","","","","","","","1B-3","Hardware Accelerator Design for CNN","<b>Seunghyun Park, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/Rk_Dpnm3Jls","");
+
+
+  
+// Digital Twin
+DB[05] = new Array("1C","color-oral-06","1C","July. 04, 2022 (Friday)","10:30 ~ 10:45","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Digital Twin-based Digital Signal Processing","IC-1","Singular Value Julyomposition: Digital Twin and Image Processing","<b>Seungmin Lee, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/B_v8miVnQO0","1D");
+
+DB[06] = new Array("","","","","","","","","1C-2","Implementation of Animated Walkthrough using Blender and Unity","<b>Haejoo Kim, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/1pZvHjXpe2g","");
+
+// AI-Enabled Algorithm
+
+DB[07] = new Array("1D","color-oral-04","1D","July. 04, 2022 (Friday)","10:45 ~ 11:30","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","AI Accelerated Embedded Software","1D-1","Embedded board OS - Structure and Operation","<b>Dongkyu Jung, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/MRixi-cw_Bs","1E");
+
+
+// Compiler Simulation
+
+DB[08] = new Array("1E","color-oral-05","1E","July. 04, 2022 (Friday)","11:30 ~ 12:00","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Middleware Compiler Simulator EDA","1E-1","ML for Low Power Compiler Optimization","<b>Juneseo Chang, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/NkR2HQ74ctE","1F");
+
+DB[09] = new Array("","","","","","","","","1E-2","Lightweighted CTS preconstruction techniques for checking clock tree synthesizable parts in RTL design time","<b>Nayoung Kwon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/LwN-MRmqO6A","");
+
+DB[10] = new Array("","","","","","","","","1E-3","Simulation of Maximum Power Point Tracking (MPPT) using Boost Converter in MATLAB-Simulink","<b>Younghun Lee, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/UXVrQ00w828","");
+
+
+DB[11] = new Array("","","","","","","","","1E-4","End of Symposium","","","","");
+
+
+
