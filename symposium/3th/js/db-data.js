377396cd46428c53edf5dd3071ff6d31492f3aa6
AI-SoC <65325744+AI-SoC@users.noreply.github.com>
2024-02-17 22:55:29 +0900

    Add files via upload

---
+++ symposium/3th/js/db-data.js
@@ -0,0 +1,60 @@
+ /*ORDER 
+NAME anchor = SESSION CODE 
+
+		0						 1							2							3			4				5				6				7									8						9					10				11			12				13			
+Session CODE / COLOR CSS / Session Name / Date / Time /  Room / Chair / Session Topic / Paper Code / Paper Title / Author / Belong / PDF / name anchor
+
+SEARCH AREA: Session Topic / Paper Title / Author & Belong  
+*/
+
+/* ARRAY 
+DB		Oral
+PS		Poster
+*/
+
+DB = new Array();
+PS = new Array();
+
+
+// Cloud Metaverse
+DB[00] = new Array("1A","color-oral-01","1A","May. 22, 2022 (Friday)","09:00 ~ 09:30","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Cloud Metaverse OS Computing","1A-1","Turing PI: Tool for Learning Microservice on a Bare-Metal Cluster","<b>Dongkyu Lee, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/twwEzqd83xc","1B");
+
+DB[01] = new Array("","","","","","","","","1A-2","TensorFlow.js Machine Learning at Web Browser' Distributed Computing","<b>Myungjin Kang, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/Yawmcw2FEgE","");
+
+
+// SoC
+DB[02] = new Array("1B","color-oral-02","1B","May. 22, 2022 (Friday)","09:30 ~ 10:15","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","System-on-Chip","1B-1","AI MCU: Neural Network-based Hardware Compensation - ADC Applications","<b>Jisu Kwon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/lUQ1bdGr2OI","1C");
+
+DB[03] = new Array("","","","","","","","","1B-2","FPGA Programming using Nexys Video and Zynq 7000 SoC ZC702","<b>Heejee Yun, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/5jhj9rN_HeU","");
+
+DB[04] = new Array("","","","","","","","","1B-3","Hardware Accelerator Design for Noise Cancellation","<b>Seunghyun Park, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/g7vBWT86c9w","");
+
+DB[05] = new Array("","","","","","","","","1B-4","Implementation of MPPT Achieving 99.9% MPPT Efficiency for photovoltaic applications in 130nm 40V BCD technology","<b>Seunghyun Park, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/sSL0qh2UbJs","");
+
+  
+// Digital Twin
+DB[06] = new Array("1C","color-oral-06","1C","May. 22, 2022 (Friday)","10:30 ~ 10:45","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Digital Twin-based Digital Signal Processing","IC-1","Non-Destructive Welding Point Detection and Welding Strength Analysis Techniques of Cylindrical Secondary Battery for Vehicles Based on Eddy Current Testing","<b>Seungmin Lee, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/bGucGrFpQZM","1D");
+
+
+DB[07] = new Array("","","","","","","","","1C-2","Fundamentals of Neural Networks","<b>Haejoo Kim, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/QAbx1vGNqLg","");
+
+// AI-Enabled Algorithm
+DB[08] = new Array("1D","color-oral-04","1D","May. 22, 2022 (Friday)","10:45 ~ 11:30","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","AI Accelerated Embedded Software","1D-1","Machine Learning base LiDAR data Semantic Classification and Masked Image Detection","<b>Dongkyu Jung, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/UdXFARWJWYY","1E");
+
+DB[09] = new Array("","","","","","","","","1D-2","Runtime Virtual Lane Prediction Based on Inverse Perspective Transformation and Machine Learning for Lane Departure Warning in Low-Power Embedded Systems","<b>Sunghoon Hong, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/ScLIaNtTAzs","");
+
+DB[10] = new Array("","","","","","","","","1D-3","Runtime Embedded SW Malfunction Detection using I/V Pattern Matching Profile","<b>Sanghoon Lee, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/mODwxF7DpDA","");
+
+
+// EDA
+DB[11] = new Array("1E","color-oral-05","1E","May. 22, 2022 (Friday)","11:30 ~ 12:00","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Middleware Simulator Compiler EDA","1E-1","Introduction to Processor Optimization Development ","<b>Nayoung Kwon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/LN1qbJut1fY","1F");
+
+DB[12] = new Array("","","","","","","","","1E-2","Applications and Countermeasures of Side Channel Attacks","<b>Juneseo Chang, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/Om9TBrXIZS8","");
+
+DB[13] = new Array("","","","","","","","","1E-3","Understanding of CDC (Clock Domain Crossing) Issue and Solutions in Digital Logic Design","<b>Juneseo Chang, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/xrJJ0ChjRw4","");
+
+
+DB[14] = new Array("","","","","","","","","1E-4","End of Symposium","","","","");
+
+
+
