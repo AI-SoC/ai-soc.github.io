a47d8f8ad020c9e6b540ce636360d432f153551c
AI-SoC <65325744+AI-SoC@users.noreply.github.com>
2024-02-17 22:30:14 +0900

    Add files via upload

---
+++ symposium/1th/js/db-data.js
@@ -0,0 +1,49 @@
+ /*ORDER 
+NAME anchor = SESSION CODE 
+
+		0						 1							2							3			4				5				6				7									8						9					10				11			12				13			
+Session CODE / COLOR CSS / Session Name / Date / Time /  Room / Chair / Session Topic / Paper Code / Paper Title / Author / Belong / PDF / name anchor
+
+SEARCH AREA: Session Topic / Paper Title / Author & Belong  
+*/
+
+/* ARRAY 
+DB		Oral
+PS		Poster
+*/
+
+DB = new Array();
+PS = new Array();
+
+
+// SoC
+DB[00] = new Array("1B","color-oral-01","1B","Mar. 23, 2022 (Friday)","09:30 ~ 10:15","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","System-on-Chip","1B-1","FPGA Implementation of CNN Modules","<b>Seunghyun Park, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/3d94OdcUHTg","1C");
+
+
+// Embedded Systems
+DB[01] = new Array("1C","color-oral-06","1C","Mar. 23, 2022 (Friday)","10:30 ~ 10:45","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Embedded Systems","IC-1","BFree - Enabling Battery-Free Sensor Prototyping with Python","<b>Myeongjin Kang, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/9BhaMO08bfI","1D");
+
+DB[02] = new Array("","","","","","","","","1C-2","FaceBit: Smart Face Masks Platform","<b>Sungho Cho, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/-pgi5FPh3xA","");
+
+DB[03] = new Array("","","","","","","","","1C-3","i.MX6 BSP Porting Guide for Camera Module","<b>Sunghoon Hong, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/hUoMdPKLYJ4","");
+
+// AI-Enabled Algorithm
+DB[04] = new Array("1D","color-oral-04","1D","Mar. 23, 2022 (Friday)","10:45 ~ 11:30","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","AI Accelerated Embedded Software","1D-1","Lidar Data Distance-Based Clustering and Semantic Classification for 2D-Based Detecting","<b>Dongkyu Jung, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/nq2SslJoUh8","1E");
+
+DB[05] = new Array("","","","","","","","","1D-2","Semantic Depth Data Transmission Reduction Techniques for Light-weighted LiDAR Signal Processing Platform","<b>Dongkyu Lee, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/DK3e6ZNTjlM","");
+
+DB[06] = new Array("","","","","","","","","1D-3","Soft Error and Control Flow Error Detection for Neural Network Application in Embedded Systems","<b>Juneseo Chang, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/EOzw0srYa7I","");
+
+
+// EDA 
+DB[07] = new Array("1E","color-oral-05","1E","Mar. 23, 2022 (Friday)","11:30 ~ 12:00","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","EDA CAD Tools Simulation","1E-1","CTS Accelerating Algorithm Of P&R Optimization","<b>Nayoung Kwon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/42rGw3ePmB8","1F");
+
+DB[08] = new Array("","","","","","","","","1E-2","Understanding Capture and Replay Environment to Simulate IC Test Situation","<b>Junghyun Ahn, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/UM45kNZ7ctY","");
+
+DB[09] = new Array("","","","","","","","","1E-3","Vivado, VITIS, Python Programming with FPGA Board","<b>Heejee Yun, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/FwhGtBzNUhU","");
+
+
+DB[10] = new Array("","","","","","","","","1E-4","End of Symposium","","","","");
+
+
+
