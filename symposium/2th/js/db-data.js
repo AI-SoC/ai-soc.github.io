009e895aa49a4f8d7bc35cf14de8e61d22910675
AI-SoC <65325744+AI-SoC@users.noreply.github.com>
2024-02-17 22:31:25 +0900

    Add files via upload

---
+++ symposium/2th/js/db-data.js
@@ -0,0 +1,57 @@
+ /*ORDER 
+NAME anchor = SESSION CODE 
+
+		0						 1							2							3			4				5				6				7									8						9					10				11			12				13			
+Session CODE / COLOR CSS / Session Name / Date / Time /  Room / Chair / Session Topic / Paper Code / Paper Title / Author / Belong / PDF / name anchor
+
+SEARCH AREA: Session Topic / Paper Title / Author & Belong  
+*/
+
+/* ARRAY 
+DB		Oral
+PS		Poster
+*/
+
+DB = new Array();
+PS = new Array();
+
+
+// Cloud Metaverse
+DB[00] = new Array("1A","color-oral-01","1A","Apr. 29, 2022 (Friday)","09:00 ~ 09:30","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Cloud Metaverse OS Computing","1A-1","Implementation of Trading Strategy and Deep Learning-based Stock Price Prediction","<b>Hyejoo Kim, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/3f9dsDxD-bc","1B");
+
+DB[01] = new Array("","","","","","","","","1A-2","Low-power Metaverse Platform through Real-time Multi-remote Streaming Video and Signal Processing based on Cloud Distributed/Cooperative Interprocessing","<b>Myungjin Kang, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/TWkNoLQNqGc","");
+
+
+// SoC
+DB[02] = new Array("1B","color-oral-02","1B","Apr. 29, 2022 (Friday)","09:30 ~ 10:15","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","System-on-Chip","1B-1","A Study of the Real-Time Object Recognition Implementation in Lightweight Embedded Processors","<b>Heejee Yun, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/zuuFvIozTwU","1C");
+
+
+DB[03] = new Array("","","","","","","","","1B-2","Convolution and FIR Filter Design with Power and Performance Consideration","<b>Seunghyun Park, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/SQGZNXHdISU","");
+
+
+  
+// Digital Twin
+DB[04] = new Array("1C","color-oral-06","1C","Apr. 29, 2022 (Friday)","10:30 ~ 10:45","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Digital Twin-based Digital Signal Processing","IC-1","A Study on Synchronized Linear Approximation Method of Multi-Channel ECG Signals","<b>Seungmin Lee, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/7TeQBEId8PM","1D");
+
+DB[05] = new Array("","","","","","","","","1C-3","Fast Adaptation Techniques of Compensation Coefficient of Active Noise Canceller.","<b>Junghyun Ahn, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/R346mKzRQlI","");
+
+
+// AI-Enabled Algorithm
+DB[06] = new Array("1D","color-oral-04","1D","Apr. 29, 2022 (Friday)","10:45 ~ 11:30","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","AI Accelerated Embedded Software","1D-1","LiDAR Data Distance-Based Clustering and Semantic Classification on ARMv7 base board","<b>Dongkyu Jung, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/QQ8l6H2vEv8","1E");
+
+DB[07] = new Array("","","","","","","","","1D-2","Semantic Depth Data Transmission Reduction Techniques for Light-weighted LiDAR Signal Processing Platform ","<b>Dongkyu Lee, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/O7yYtIQjKmE","");
+
+DB[08] = new Array("","","","","","","","","1D-3","Object Detection Guide with CUDA in WSL2","<b>Sunghoon Hong, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/kgYwczbq_oY","");
+
+
+// Compiler Simulation
+
+DB[09] = new Array("1E","color-oral-05","1E","Apr. 29, 2022 (Friday)","11:30 ~ 12:00","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","OS Middleware Compiler Simulator","1E-1","Cache-based Side-Channel Attacks and Code-level Countermeasures","<b>Juneseo Chang, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/nrDBRpE5gBU","1F");
+
+DB[10] = new Array("","","","","","","","","1E-2","Qflow Verification using CTS Accelerating Algorithm","<b>Nayoung Kwon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/TGJ-PYETZtM","");
+
+
+DB[11] = new Array("","","","","","","","","1E-2","End of Symposium","","","","");
+
+
+
