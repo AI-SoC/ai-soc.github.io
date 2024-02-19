7f906d75c8c2faaa24165f44308cb333be05ffda
AI-SoC <65325744+AI-SoC@users.noreply.github.com>
2024-02-19 17:42:39 +0900

    Add files via upload

---
+++ symposium/9th/js/db-data.js
@@ -0,0 +1,56 @@
+ /*ORDER 
+NAME anchor = SESSION CODE 
+
+		0						 1							2							3			4				5				6				7									8						9					10				11			12				13			
+Session CODE / COLOR CSS / Session Name / Date / Time /  Room / Chair / Session Topic / Paper Code / Paper Title / Author / Belong / PDF / name anchor
+
+SEARCH AREA: Session Topic / Paper Title / Author & Belong  
+*/
+
+/* ARRAY 
+DB		Oral
+PS		Poster
+*/
+
+DB = new Array();
+PS = new Array();
+
+
+// Cloud Metaverse
+DB[00] = new Array("1A","color-oral-01","1A","Feb. 28, 2024 (Friday)","09:00 ~ 09:30","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Cloud Metaverse Algorithm","1A-1","Cloud Memory Enabled Code Generation via Online Computing for Seamless Edge AI Operation","<b>Myeongjin Kang, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","1B");
+
+DB[01] = new Array("","","","","","","","","1A-2","Self-Organizing Movement Control for Large-Scaled Autonomous Vehicle Collision Congestion Mitigation","<b>Youngwon Jeon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
+
+
+// SoC
+DB[02] = new Array("1B","color-oral-02","1B","Feb. 28, 2024 (Friday)","09:30 ~ 10:30","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","System-on-Chip","1B-1","Heterogeneous Host-Accelerator Linking Layer for Resource Fully-Utilization","<b>Jisu Kwon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","1C");
+
+DB[03] = new Array("","","","","","","","","1B-2","Pattern based Input Data Sorting in CSNN Hardware Accelerator for Low Power High Speed in Light-weighted Embedded Board","<b>Heejee Yun, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
+
+DB[04] = new Array("","","","","","","","","1B-3","Enabling Seamless AI Acceleration","<b>Seunghyun Park, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
+
+DB[05] = new Array("","","","","","","","","1B-4","FPGA Implementation of CNN Accelerated RISC-V Co-Processors","<b>Minkyu Jang, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
+
+
+  
+// Digital Twin
+DB[06] = new Array("1C","color-oral-06","1C","Feb. 28, 2024 (Friday)","10:30 ~ 10:45","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Digital Twin-based Digital Signal Processing","IC-1","Integrating and Simulating Python Algorithms within Simulink Systems","<b>Seungmin Lee, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","1D");
+
+
+// AI-Enabled Algorithm
+
+
+DB[07] = new Array("1D","color-oral-04","1D","Feb. 28, 2024 (Friday)","10:45 ~ 11:00","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","AI Accelerated Embedded Software","1D-1","Multi-Camera Interoperable Emulation Framework using Embedded Edge-Cloud AI Computing for Autonomous Vehicle Driving","<b>Hyunjung Lee, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","1E");
+
+
+// Compiler Simulation
+
+DB[08] = new Array("1E","color-oral-05","1E","Feb. 28, 2024 (Friday)","11:00 ~ 11:30","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Hypervisor Middleware Compiler Simulator","1E-1","Evaluation of LMStr with Paging based on gem5 & Analysis of gem5","<b>Gihyun Jeon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","1F");
+
+DB[09] = new Array("","","","","","","","","1E-2","Dynamic Round Robin on Hypervisor","<b>Minjung Kim, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
+
+
+DB[10] = new Array("","","","","","","","","1E-3","End of Symposium","","","","");
+
+
+
