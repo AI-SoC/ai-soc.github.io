7d19a9460c542d9de5981951fcf5335ceef215f9
AI-SoC <65325744+AI-SoC@users.noreply.github.com>
2024-03-02 13:04:04 +0900

    Add files via upload

--- symposium/9th/js/db-data.js
+++ symposium/9th/js/db-data.js
@@ -17,19 +17,19 @@ PS = new Array();
 
 
 // Cloud Metaverse
-DB[00] = new Array("1A","color-oral-01","1A","Feb. 28, 2024 (Friday)","09:00 ~ 09:30","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Cloud Metaverse Algorithm","1A-1","Cloud Memory Enabled Code Generation via Online Computing for Seamless Edge AI Operation","<b>Myeongjin Kang, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","1B");
+DB[00] = new Array("1A","color-oral-01","1A","Feb. 28, 2024 (Friday)","09:00 ~ 09:30","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Cloud Metaverse Algorithm","1A-1","Cloud Memory Enabled Code Generation via Online Computing for Seamless Edge AI Operation","<b>Myeongjin Kang, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/Qhbyw9txM0E","1B");
 
-DB[01] = new Array("","","","","","","","","1A-2","Self-Organizing Movement Control for Large-Scaled Autonomous Vehicle Collision Congestion Mitigation","<b>Youngwon Jeon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
+DB[01] = new Array("","","","","","","","","1A-2","Self-Organizing Movement Control for Large-Scaled Autonomous Vehicle Collision Congestion Mitigation","<b>Youngwon Jeon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/k1XcqMrHAvA","");
 
-DB[02] = new Array("","","","","","","","","1A-3","Adaptive Air Conditioner Control by Agent-based Dynamic Simulation","<b>Yujeong Son, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
+DB[02] = new Array("","","","","","","","","1A-3","Adaptive Air Conditioner Control by Agent-based Dynamic Simulation","<b>Yujeong Son, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/_1thv5jCpIY","");
 
 
 // SoC
-DB[03] = new Array("1B","color-oral-02","1B","Feb. 28, 2024 (Friday)","09:30 ~ 10:30","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","System-on-Chip","1B-1","Heterogeneous Host-Accelerator Linking Layer for Resource Fully-Utilization","<b>Jisu Kwon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","1C");
+DB[03] = new Array("1B","color-oral-02","1B","Feb. 28, 2024 (Friday)","09:30 ~ 10:30","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","System-on-Chip","1B-1","Heterogeneous Host-Accelerator Linking Layer for Resource Fully-Utilization","<b>Jisu Kwon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/c4ih0LwXoQE","1C");
 
-DB[04] = new Array("","","","","","","","","1B-2","Pattern based Input Data Sorting in CSNN Hardware Accelerator for Low Power High Speed in Light-weighted Embedded Board","<b>Heejee Yun, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
+DB[04] = new Array("","","","","","","","","1B-2","Pattern based Input Data Sorting in CSNN Hardware Accelerator for Low Power High Speed in Light-weighted Embedded Board","<b>Heejee Yun, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/bT88rd-kvBU","");
 
-DB[05] = new Array("","","","","","","","","1B-3","Enabling Seamless AI Acceleration","<b>Seunghyun Park, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
+DB[05] = new Array("","","","","","","","","1B-3","Enabling Seamless AI Acceleration","<b>Seunghyun Park, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/WONnOsengzo","");
 
   
 // Digital Twin
@@ -39,14 +39,14 @@ DB[06] = new Array("1C","color-oral-06","1C","Feb. 28, 2024 (Friday)","10:30 ~ 1
 // AI-Enabled Algorithm
 
 
-DB[07] = new Array("1D","color-oral-04","1D","Feb. 28, 2024 (Friday)","10:45 ~ 11:00","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","AI Accelerated Embedded Software","1D-1","Multi-Camera Interoperable Emulation Framework using Embedded Edge-Cloud AI Computing for Autonomous Vehicle Driving","<b>Hyunjung Lee, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","1E");
+DB[07] = new Array("1D","color-oral-04","1D","Feb. 28, 2024 (Friday)","10:45 ~ 11:00","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","AI Accelerated Embedded Software","1D-1","Multi-Camera Interoperable Emulation Framework using Embedded Edge-Cloud AI Computing for Autonomous Vehicle Driving","<b>Hyunjung Lee, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/Lhx2vVfdb9Q","1E");
 
 
 // Compiler Simulation
 
-DB[08] = new Array("1E","color-oral-05","1E","Feb. 28, 2024 (Friday)","11:00 ~ 11:30","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Hypervisor Middleware Compiler Simulator","1E-1","Evaluation of LMStr with Paging based on gem5 & Analysis of gem5","<b>Gihyun Jeon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","1F");
+DB[08] = new Array("1E","color-oral-05","1E","Feb. 28, 2024 (Friday)","11:00 ~ 11:30","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Hypervisor Middleware Compiler Simulator","1E-1","Evaluation of LMStr with Paging based on gem5 & Analysis of gem5","<b>Gihyun Jeon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/X24yLGpEK8c","1F");
 
-DB[09] = new Array("","","","","","","","","1E-2","Dynamic Round Robin on Hypervisor","<b>Minjung Kim, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
+DB[09] = new Array("","","","","","","","","1E-2","Dynamic Round Robin on Hypervisor","<b>Minjung Kim, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/ucZEOXVy9Kw","");
 
 
 DB[10] = new Array("","","","","","","","","1E-3","End of Symposium","","","","");
