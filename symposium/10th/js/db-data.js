3c47aa5c95894b62c551d86a92cd9aa490ae919f
AI-SoC <65325744+AI-SoC@users.noreply.github.com>
2024-05-07 19:24:02 +0900

    Add files via upload

--- symposium/10th/js/db-data.js
+++ symposium/10th/js/db-data.js
@@ -17,44 +17,44 @@ PS = new Array();
 
 
 // Cloud Metaverse
-DB[00] = new Array("1A","color-oral-01","1A","May. 2, 2024 (Thursday)","09:00 ~ 09:30","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Cloud Metaverse Algorithm","1A-1","Edge AI Accuracy Improving through External Situational Awareness-based Retraining","<b>Myeongjin Kang, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/Qhbyw9txM0E","1B");
+DB[00] = new Array("1A","color-oral-01","1A","May. 2, 2024 (Thursday)","09:00 ~ 09:30","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Cloud Metaverse Algorithm","1A-1","Edge AI Accuracy Improving through External Situational Awareness-based Retraining","<b>Myeongjin Kang, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/yBrF9BH9B-A","1B");
 
-DB[01] = new Array("","","","","","","","","1A-2","Dodging adaptive A* algorithm for running time reduction and application on real map","<b>Youngwon Jeon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/k1XcqMrHAvA","");
+DB[01] = new Array("","","","","","","","","1A-2","Dodging adaptive A* algorithm for running time reduction and application on real map","<b>Youngwon Jeon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/lHMsjo7fVLE","");
 
-DB[02] = new Array("","","","","","","","","1A-3","Network-based Structured Aware Collaborative Control","<b>Suhwan Lee, Daejin Park</b> (LG Electronics, Kyungpook National University, Korea)<br>","","https://youtu.be/_1thv5jCpIY","");
+DB[02] = new Array("","","","","","","","","1A-3","Network-based Structured Aware Collaborative Control","<b>Suhwan Lee, Daejin Park</b> (LG Electronics, Kyungpook National University, Korea)<br>","","https://youtu.be/q483NmURa3I","");
 
 
 // SoC
-DB[03] = new Array("1B","color-oral-02","1B","May. 2, 2024 (Thursday)","09:30 ~ 10:30","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","System-on-Chip","1B-1","Evaluation of the host overhead from heterogeneous accelerator control libraries & accelerator utilization","<b>Jisu Kwon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/c4ih0LwXoQE","1C");
+DB[03] = new Array("1B","color-oral-02","1B","May. 2, 2024 (Thursday)","09:30 ~ 10:30","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","System-on-Chip","1B-1","Evaluation of the host overhead from heterogeneous accelerator control libraries & accelerator utilization","<b>Jisu Kwon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/Wukm2R3Rd64","1C");
 
-DB[04] = new Array("","","","","","","","","1B-2","Pattern-based Self-supervised Learning Accelerator models using CSNN for Low-power, high-efficiency","<b>Heejee Yun, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/bT88rd-kvBU","");
+DB[04] = new Array("","","","","","","","","1B-2","Pattern-based Self-supervised Learning Accelerator models using CSNN for Low-power, high-efficiency","<b>Heejee Yun, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/6CqES_GPtxg","");
 
-DB[05] = new Array("","","","","","","","","1B-3","Improvements in Computational Performance and Connection of AI Accelerators","<b>Seunghyun Park, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/WONnOsengzo","");
+DB[05] = new Array("","","","","","","","","1B-3","Improvements in Computational Performance and Connection of AI Accelerators","<b>Seunghyun Park, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/cpgyITrt7uo","");
 
   
 // Digital Twin
-DB[06] = new Array("1C","color-oral-06","1C","May. 2, 2024 (Thursday)","10:30 ~ 10:45","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Digital Twin-based Digital Signal Processing","IC-1","Projection Data Analysis and Noise Suppression Filter Algorithm for Height Level Sensor","<b>Seungmin Lee, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","1D");
+DB[06] = new Array("1C","color-oral-06","1C","May. 2, 2024 (Thursday)","10:30 ~ 10:45","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Digital Twin-based Digital Signal Processing","IC-1","Projection Data Analysis and Noise Suppression Filter Algorithm for Height Level Sensor","<b>Seungmin Lee, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/NJpDwB1Ckdc","1D");
 
 
-DB[07] = new Array("","","","","","","","","1C-2","Expanding simulation environment through Verilog PLI","<b>Yujeong Son, Daejin Park</b> (LG Display, Kyungpook National University, Korea)<br>","","https://youtu.be/WONnOsengzo","");
+DB[07] = new Array("","","","","","","","","1C-2","Expanding simulation environment through Verilog PLI","<b>Yujeong Son, Daejin Park</b> (LG Display, Kyungpook National University, Korea)<br>","","https://youtu.be/pF2fHetXAKA","");
 
 // AI-Enabled Algorithm
 
 
-DB[08] = new Array("1D","color-oral-04","1D","May. 2, 2024 (Thursday)","10:45 ~ 11:00","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","AI Accelerated Embedded Software","1D-1","3D Mapping using Stereo Vision","<b>Hyunjung Lee, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/Lhx2vVfdb9Q","1E");
+DB[08] = new Array("1D","color-oral-04","1D","May. 2, 2024 (Thursday)","10:45 ~ 11:00","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","AI Accelerated Embedded Software","1D-1","3D Mapping using Stereo Vision","<b>Hyunjung Lee, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/74kuJAkrvoc","1E");
 
-DB[09] = new Array("","","","","","","","","1D-2","Docking System for Autonomous Mobile Robots using 2D LiDAR","<b>Sunghun Hong, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/WONnOsengzo","");
+DB[09] = new Array("","","","","","","","","1D-2","Docking System for Autonomous Mobile Robots using 2D LiDAR","<b>Sunghun Hong, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/Qihd1Y8yh30","");
 
-DB[10] = new Array("","","","","","","","","1D-3","Automated Targeting control system with Yolo","<b>Jaehun Kim, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/WONnOsengzo","");
+DB[10] = new Array("","","","","","","","","1D-3","Automated Targeting control system with Yolo","<b>Jaehun Kim, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/AHUf3zcgJEk","");
 
 
 // Compiler Simulation
 
-DB[11] = new Array("1E","color-oral-05","1E","May. 2, 2024 (Thursday)","11:00 ~ 11:30","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Hypervisor, Compiler","1E-1","Compiler Optimization for Scratchpad Memory Allocation","<b>Gihyun Jeon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/X24yLGpEK8c","1F");
+DB[11] = new Array("1E","color-oral-05","1E","May. 2, 2024 (Thursday)","11:00 ~ 11:30","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Hypervisor, Compiler","1E-1","Compiler Optimization for Scratchpad Memory Allocation","<b>Gihyun Jeon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/Fz1NZGpK67k","1F");
 
-DB[12] = new Array("","","","","","","","","1E-2","Hypervised MCU structure for optimizing AI execution","<b>Minjung Kim, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/ucZEOXVy9Kw","");
+DB[12] = new Array("","","","","","","","","1E-2","Hypervised MCU structure for optimizing AI execution","<b>Minjung Kim, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/ren1e_5YY3Y","");
 
-DB[13] = new Array("","","","","","","","","1E-2","On-cloud Code Streaming-based Firmware Partial Update for Embedded Edge Device","<b>Dongkyu Lee, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/ucZEOXVy9Kw","");
+DB[13] = new Array("","","","","","","","","1E-2","On-cloud Code Streaming-based Firmware Partial Update for Embedded Edge Device","<b>Dongkyu Lee, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/obgKx6L1Wco","");
 
 
 DB[14] = new Array("","","","","","","","","1E-3","End of Symposium","","","","");
