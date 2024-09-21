81fc42d2c91d17c0f796bbafbc7d745bb124f67b
AI-SoC <65325744+AI-SoC@users.noreply.github.com>
2024-09-21 10:45:00 +0900

    Add files via upload

--- symposium/11th/js/db-data.js
+++ symposium/11th/js/db-data.js
@@ -17,37 +17,44 @@ PS = new Array();
 
 
 // Cloud Metaverse
-DB[00] = new Array("1A","color-oral-01","1A","Sept. 6, 2024 (Friday)","15:00 ~ 15:15","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Cloud Metaverse Algorithm","1A-1","Fully edge-server parallel execution with code analysis scheduling","<b>Myeongjin Kang, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","1B");
+DB[00] = new Array("1A","color-oral-01","1A","Sept. 6, 2024 (Friday)","15:00 ~ 15:15","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Cloud Metaverse Algorithm","1A-1","Fully edge-server parallel execution with code analysis scheduling","<b>Myeongjin Kang, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/_b_uaTP7-3A","1B");
+
+DB[01] = new Array("","","","","","","","","1A-2","Traffic Arbiter Effect using Socket Programming","<b>Youngwon Jeon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/73guvcEWAvM","");
 
 
 // SoC
-DB[01] = new Array("1B","color-oral-02","1B","Sept. 6, 2024 (Friday)","15:15 ~ 16:00","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","System-on-Chip","1B-1","A Host and Accelerator Decoupling Perspective","<b>Jisu Kwon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","1C");
+DB[02] = new Array("1B","color-oral-02","1B","Sept. 6, 2024 (Friday)","15:15 ~ 16:00","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","System-on-Chip","1B-1","A Host and Accelerator Decoupling Perspective","<b>Jisu Kwon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/iZkGBEWdVHs","1C");
 
-DB[02] = new Array("","","","","","","","","1B-2","Model Flexible Self-supervised Learning NPU and Scheduling Techniques for Autonomous Driving","<b>Heejee Yun, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
+DB[03] = new Array("","","","","","","","","1B-2","Model Flexible Self-supervised Learning NPU and Scheduling Techniques for Autonomous Driving","<b>Heejee Yun, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/uYSaHq-C6Ok","");
 
-DB[03] = new Array("","","","","","","","","1B-3","Improvements in Bit-Separable Multiplier and Off-Chip Network Architecture in AI Accelerators","<b>Seunghyun Park, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
+DB[04] = new Array("","","","","","","","","1B-3","Improvements in Bit-Separable Multiplier and Off-Chip Network Architecture in AI Accelerators","<b>Seunghyun Park, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/HD8Mdy4NBCk","");
+
+DB[05] = new Array("","","","","","","","","1B-4","ECC Module with Improved Performance and Area Efficiency","<b>Chihun Hwang, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/tEUCDcDDRow","");
+
+DB[06] = new Array("","","","","","","","","1B-4","Low-Power High Speed CNN Accelerator with Matrix Reordering Techniques for Small Footprint Memory Access","<b>Hoseung Kim, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/PJEaKy633ZA","");
 
   
 // Digital Twin
-DB[04] = new Array("1C","color-oral-06","1C","Sept. 6, 2024 (Friday)","16:00 ~ 16:30","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Digital Twin, Signal Processing","1C-1","3D Data Analysis and Reconstruction Using PCA and Ellipse Fitting for Height Level Sensor","<b>Seungmin Lee, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","1D");
+DB[07] = new Array("1C","color-oral-06","1C","Sept. 6, 2024 (Friday)","16:00 ~ 16:30","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Digital Twin, Signal Processing","1C-1","3D Data Analysis and Reconstruction Using PCA and Ellipse Fitting for Height Level Sensor","<b>Seungmin Lee, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","1D");
 
-DB[05] = new Array("","","","","","","","","1C-2","Compressive Sensing Techniques for Efficient Importance Sampling","<b>Seongho Cho, Daejin Park</b> (LG Display, Kyungpook National University, Korea)<br>","","","");
+DB[08] = new Array("","","","","","","","","1C-2","Compressive Sensing Techniques for Efficient Importance Sampling","<b>Seongho Cho, Daejin Park</b> (LG Display, Kyungpook National University, Korea)<br>","","","");
 
 // AI-Enabled Algorithm
-DB[06] = new Array("1D","color-oral-04","1D","Sept. 6, 2024 (Friday)","16:30 ~ 17:15","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","AI Accelerated Embedded Software","1D-1","Adaptive Auto Parking Assistance System with PID Controller","<b>Hyunjung Lee, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","1E");
+DB[09] = new Array("1D","color-oral-04","1D","Sept. 6, 2024 (Friday)","16:30 ~ 17:15","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","AI Accelerated Embedded Software","1D-1","Adaptive Auto Parking Assistance System with PID Controller","<b>Hyunjung Lee, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","1E");
 
-DB[07] = new Array("","","","","","","","","1D-2","Automotive Flow Control for Large-Scaled Collaborative Congestion Mitigation","<b>Youngwon Jeon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
+DB[10] = new Array("","","","","","","","","1D-2","Automotive Flow Control for Large-Scaled Collaborative Congestion Mitigation","<b>Youngwon Jeon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
 
-DB[08] = new Array("","","","","","","","","1D-3","Fast Object Tracking using Jetson-Tricore Hybrid Integration with Look Ahead Movement Estimator","<b>Jaehun Kim, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
+DB[11] = new Array("","","","","","","","","1D-3","Fast Object Tracking using Jetson-Tricore Hybrid Integration with Look Ahead Movement Estimator","<b>Jaehun Kim, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/tRWa049spNY","");
 
 
 // Compiler Simulation
-DB[09] = new Array("1E","color-oral-05","1E","Sept. 6, 2024 (Friday)","17:15 ~ 17:45","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Hypervisor, Compiler","1E-1","Optimizing Multithreaded Access to Global Variables in SPM through Compiler-Enhanced Dependency Analysis","<b>Gihyun Jeon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","1F");
+DB[12] = new Array("1E","color-oral-05","1E","Sept. 6, 2024 (Friday)","17:15 ~ 17:45","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Hypervisor, Compiler","1E-1","Optimizing Multithreaded Access to Global Variables in SPM through Compiler-Enhanced Dependency Analysis","<b>Gihyun Jeon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","1F");
 
-DB[10] = new Array("","","","","","","","","1E-2","Hypervising Techniques in Jailhouse Hypervisor with Erika RTOS","<b>Minjung Kim, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
+DB[13] = new Array("","","","","","","","","1E-2","Hypervising Techniques in Jailhouse Hypervisor with Erika RTOS","<b>Minjung Kim, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/xg0ciC38WJE","");
+DB[14] = new Array("","","","","","","","","1E-3","Intermittent, Fast, Partial Updatable OTA Update Techniques","<b>Janghun Lee, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/OFsJlEoyAjY","");
 
 
-DB[11] = new Array("","","","","","","","","1E-3","End of Symposium","","","","");
+DB[15] = new Array("","","","","","","","","1E-3","End of Symposium","","","","");
 
 
 
