9d7194678b6211193b8d20e199f8087510740f4d
AI-SoC <65325744+AI-SoC@users.noreply.github.com>
2024-10-10 10:54:31 +0900

    Add files via upload

---
+++ symposium/future_automotive_1st_ws/js/db-data.js
@@ -0,0 +1,45 @@
+ /*ORDER 
+NAME anchor = SESSION CODE 
+
+		0						 1							2							3			4				5				6				7									8						9					10				11			12				13			
+Session CODE / COLOR CSS / Session Name / Date / Time /  Room / Chair / Session Topic / Paper Code / Paper Title / Author / Belong / PDF / name anchor
+
+SEARCH AREA: Session Topic / Paper Title / Author & Belong  
+*/
+
+/* ARRAY 
+DB		Oral
+PS		Poster
+*/
+
+DB = new Array();
+PS = new Array();
+
+
+// Cloud Metaverse
+DB[00] = new Array("1A","color-oral-01","1A","Oct. 10, 2024 (Thursday)","15:00 ~ 15:30","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Cloud Metaverse Algorithm for Automotive Applications","1A-1","Fully edge-server parallel execution with code analysis scheduling","<b>Myeongjin Kang</b> (Kyungpook National University, Korea)<br>","","","1B");
+
+DB[01] = new Array("","","","","","","","","1A-2","Traffic Arbiter Effect using Socket Programming","<b>Youngwon Jeon</b> (Kyungpook National University, Korea)<br>","","","");
+
+
+  
+// Digital Twin
+DB[02] = new Array("1B","color-oral-06","1B","Oct. 10, 2024 (Thursday)","15:30 ~ 16:00","Room A","Chair: Jeongsik Choi (Kyungpook National University, Korea)","Digital Twin, Signal Processing for Automotive Applications","1C-1","3D Data Analysis and Reconstruction Using PCA and Ellipse Fitting for Height Level Sensor","<b>Seungmin Lee</b> (Kyungpook National University, Korea)<br>","","","1C");
+
+DB[03] = new Array("","","","","","","","","1B-2","Adaptive Auto Parking Assistance System with PID Controller","<b>Hyunjung Lee</b> (Kyungpook National University, Korea)<br>","","","");
+
+DB[04] = new Array("","","","","","","","","1B-3","Fast Object Tracking using Jetson-Tricore Hybrid Integration with Look Ahead Movement Estimator","<b>Jaehun Kim</b> (Kyungpook National University, Korea)<br>","","","");
+
+
+// Compiler Simulation
+DB[05] = new Array("1C","color-oral-05","1C","Oct. 10, 2024 (Thursday)","16:00 ~ 18:00","Room A","Chair: Jeonghun Cho (Kyungpook National University, Korea)","Hypervisor, Compiler","1E-1","Optimizing Multithreaded Access to Global Variables in SPM through Compiler-Enhanced Dependency Analysis","<b>Gihyun Jeon</b> (Kyungpook National University, Korea)<br>","","","1F");
+
+DB[06] = new Array("","","","","","","","","1C-2","Hypervising Techniques in Jailhouse Hypervisor with Erika RTOS","<b>Minjung Kim</b> (Kyungpook National University, Korea)<br>","","","");
+
+DB[07] = new Array("","","","","","","","","1C-3","Intermittent, Fast, Partial Updatable OTA Update Techniques","<b>Janghun Lee</b> (Kyungpook National University, Korea)<br>","","","");
+
+
+DB[08] = new Array("","","","","","","","","1D-3","End of Symposium","","","","");
+
+
+
