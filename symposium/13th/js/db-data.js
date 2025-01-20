bd2dd2615ee4b740933b13d6dc7ecdad9eece56e
AI-SoC <65325744+AI-SoC@users.noreply.github.com>
2025-01-21 00:07:23 +0900

    Add files via upload

---
+++ symposium/13th/js/db-data.js
@@ -0,0 +1,51 @@
+ /*ORDER 
+NAME anchor = SESSION CODE 
+
+		0						 1							2							3			4				5				6				7									8						9					10				11			12				13			
+Session CODE / COLOR CSS / Session Name / Date / Time /  Room / Chair / Session Topic / Paper Code / Paper Title / Author / Belong / PDF / name anchor
+
+SEARCH AREA: Session Topic / Paper Title / Author & Belong  
+*/
+
+/* ARRAY 
+DB		Oral
+PS		Poster
+*/
+
+DB = new Array();
+PS = new Array();
+
+
+// Cloud Metaverse
+DB[00] = new Array("1A","color-oral-01","1A","Nov. 29, 2024 (Friday)","15:00 ~ 15:15","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Cloud Metaverse Algorithm","1A-1","Multi-Core RISC-V Design with ALVEO FPGA Accelerator and Clustering","<b>Myeongjin Kang, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","1B");
+
+
+// SoC
+DB[01] = new Array("1B","color-oral-02","1B","Nov. 29, 2024 (Friday)","15:15 ~ 16:00","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","System-on-Chip","1B-1","Multi-Dimensional Inference Streamlining for Resilience AI","<b>Jisu Kwon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","1C");
+
+DB[02] = new Array("","","","","","","","","1B-2","Strategies and Innovative Approaches for Deep Learning End-to-End Optimization","<b>Heejee Yun, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
+
+DB[03] = new Array("","","","","","","","","1B-3","Leveraging Output Activation Sparsity in Attention","<b>Seunghyun Park, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
+
+  
+// Digital Twin
+DB[04] = new Array("1C","color-oral-06","1C","Nov. 29, 2024 (Friday)","16:00 ~ 16:30","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Digital Twin, Signal Processing","1C-1","Digial Twin-based Analysis for Non-Destructive Inspection","<b>Seungmin Lee, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","1D");
+
+DB[05] = new Array("","","","","","","","","1C-2","Connectivity MCU Technologies for Display Interfaces","<b>Seongho Cho, Daejin Park</b> (LG Display, Kyungpook National University, Korea)<br>","","","");
+
+// AI-Enabled Algorithm
+DB[06] = new Array("1D","color-oral-04","1D","Nov. 29, 2024 (Friday)","16:30 ~ 17:15","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","AI Accelerated Embedded Software","1D-1","Threaded Adaptive PID Controller","<b>Hyunjung Lee, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","1E");
+
+
+// Compiler Simulation
+DB[07] = new Array("1E","color-oral-05","1E","Nov. 29, 2024 (Friday)","17:15 ~ 17:45","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Hypervisor, Compiler","1E-1","Access Frequency-Driven Data Placement Optimization for TriCore Architecture using LLVM Passes","<b>Gihyun Jeon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","1F");
+
+DB[08] = new Array("","","","","","","","","1E-2","Reconfigurable Built-In Test Unit","<b>Junghyun An, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
+
+DB[09] = new Array("","","","","","","","","1E-3","Leveraging Flash Memory for Intermittent Computing","<b>Minjung Kim, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
+
+
+DB[10] = new Array("","","","","","","","","1E-3","End of Symposium","","","","");
+
+
+
