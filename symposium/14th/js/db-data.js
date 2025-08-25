dc64693a7ea3d8e58d48a8aa7aff4797f2d85834
AI-SoC <65325744+AI-SoC@users.noreply.github.com>
2025-08-25 22:31:00 +0900

    Add files via upload

---
+++ symposium/14th/js/db-data.js
@@ -0,0 +1,65 @@
+ /*ORDER 
+NAME anchor = SESSION CODE 
+
+		0						 1							2							3			4				5				6				7									8						9					10				11			12				13			
+Session CODE / COLOR CSS / Session Name / Date / Time /  Room / Chair / Session Topic / Paper Code / Paper Title / Author / Belong / PDF / name anchor
+
+SEARCH AREA: Session Topic / Paper Title / Author & Belong  
+*/
+
+/* ARRAY 
+DB		Oral
+PS		Poster
+*/
+
+DB = new Array();
+PS = new Array();
+
+
+// Cloud Metaverse
+
+DB[00] = new Array("1A","color-oral-01","1A","Mar. 28, 2025 (Friday)","15:15 ~ 16:00","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Large-Scaled System Integration","1B-1","Reconfiruable learning accelerator for incremental learning","<b>Myeongjin Kang, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","1C");
+
+DB[01] = new Array("","","","","","","","","1A-2","OpenGL ES Tutorial","<b>Jaehyun Park, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
+
+
+// SoC
+DB[02] = new Array("1B","color-oral-02","1B","Mar. 28, 2025 (Friday)","15:15 ~ 16:00","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","System-on-Chip","1B-1","Efficient Hierarchical Sensor Modeling using Protocol Translator","<b>Jisu Kwon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","1C");
+
+DB[03] = new Array("","","","","","","","","1B-2","Self-Optimizing Edge AI for Autonomous Vehicles with Real-Time Adaptive Learning","<b>Heejee Yun, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
+
+DB[04] = new Array("","","","","","","","","1B-3","BSM-ReLUFormer & Compressor Tree Designer","<b>Seunghyun Park, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
+
+DB[05] = new Array("","","","","","","","","1B-4","Linear Footprint Memory Access Interface for Low Power CNN accelerator","<b>Hoseong Kim, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
+
+DB[06] = new Array("","","","","","","","","1B-5","Pilot study for SoC defect detection","<b>Junghyun An, Daejin Park</b> (Samsung Electronics, Kyungpook National University, Korea)<br>","","","");
+
+DB[07] = new Array("","","","","","","","","1B-6","TRU-net based NPU Design for Real-time Denoising","<b>Yonghun Lee, Daejin Park</b> (Gaonchips, Kyungpook National University, Korea)<br>","","","");
+
+  
+// Digital Twin and DSP
+DB[08] = new Array("1C","color-oral-06","1C","Mar. 28, 2025 (Friday)","16:00 ~ 16:30","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Digital Twin, Signal Processing","1C-1","Study on GMM-Based Segmentation of Defects in Bottom Images of Automotive Secondary Batteries","<b>Seungmin Lee, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","1D");
+
+DB[09] = new Array("","","","","","","","","1C-2","Deep Reinforcement Learning-Based Adaptive Sliding Mode Control for Vehicle Stability Enhancement","<b>Hyunjung Lee, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
+
+DB[10] = new Array("","","","","","","","","1C-3","Network-based Structured Aware Callaborative IoT Devices Control","<b>Suhwan Lee, Daejin Park</b> (LG Electronics, Kyungpook National University, Korea)<br>","","","");
+
+DB[11] = new Array("","","","","","","","","1C-4","Rotation Angle Estimation using Hall Sensor IC","<b>Jaekyung Pi, Daejin Park</b> (Kyungchang Industry, Kyungpook National University, Korea)<br>","","","");
+
+// AI-Enabled Accelerated Embedded SW & Algorithm
+DB[12] = new Array("1D","color-oral-04","1D","Mar. 28, 2025 (Friday)","16:30 ~ 17:15","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","AI Accelerated Embedded Software","1D-1","Symmetric Multi-Processing RTOS for the TriCore Architecture","<b>Minjung Kim, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","1E");
+
+DB[13] = new Array("","","","","","","","","1D-2","Pallet Detection for Autonomous Forklift Docking using ROS Gazebo Simulation","<b>Sunghoon Hong, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
+DB[14] = new Array("","","","","","","","","1D-3","Overview of Automotive Battery Management Systems","<b>Gihyun Seong, Daejin Park</b> (SL, Kyungpook National University, Korea)<br>","","","");
+
+// Compiler Simulation, Software Framework
+DB[15] = new Array("1E","color-oral-05","1E","Mar. 28, 2025 (Friday)","17:15 ~ 17:45","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Compiler, Simulator, Software Framework", "1E-1", "Efficient Memory Allocation SPM(TCM) in TriCore(Cortex-M7)","<b>Gihyun Jeon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","1F");
+
+DB[16] = new Array("","","","","","","","","1E-2","Docker-Based Embedded Software Management with Dynamic Library Techniques for Adaptive Control System","<b>Janghun Lee, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
+
+
+
+DB[17] = new Array("","","","","","","","","1E-3","End of Symposium","","","","");
+
+
+
