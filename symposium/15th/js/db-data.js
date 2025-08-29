3dc979cfdbd984352530aef1a2b8ac6bb9269889
AI-SoC <65325744+AI-SoC@users.noreply.github.com>
2025-08-29 09:15:59 +0900

    Add files via upload

--- symposium/15th/js/db-data.js
+++ symposium/15th/js/db-data.js
@@ -23,46 +23,49 @@ DB[00] = new Array("1A","color-oral-01","1A","Aug. 28, 2025 (Friday)","10:00","R
 DB[01] = new Array("","","","","","","","","1A-2","Transfer Learning using Lazy Evaluation for CNN Acceleration","<b>Sunghoon Hong, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
 
 DB[02] = new Array("","","","","","","","","1A-3","Multi-threaded Battery Management System on Tricore Multicore Arcitecture","<b>Gihyeon Seong, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
+DB[03] = new Array("","","","","","","","","1A-4","Performance Analysis of Program under Varying Input Demands","<b>Seungwon Bang, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
+DB[04] = new Array("","","","","","","","","1A-5","Evaluating the Impact of Access Patterns on Disk-Based Hash Table Performance with Various I/O Models","<b>Taejune Yun, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
+DB[05] = new Array("","","","","","","","","1A-6","Evaluating Lazy Loading Strategies for Large-Scale Data with Skewed Distribution","<b>Kwanwoo Kwon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
 
 
 // SoC
-DB[03] = new Array("1B","color-oral-02","1B","Aug. 28, 2025 (Friday)","11:00","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","System-on-Chip","1B-1","Beyond Monolithic RL: Delay-Aware Hierarchical RL for On-Chip Multi-Knob Control with Predictive Safety","<b>Heejee Yun, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","1C");
+DB[06] = new Array("1B","color-oral-02","1B","Aug. 28, 2025 (Friday)","11:00","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","System-on-Chip","1B-1","Beyond Monolithic RL: Delay-Aware Hierarchical RL for On-Chip Multi-Knob Control with Predictive Safety","<b>Heejee Yun, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","1C");
 
-DB[04] = new Array("","","","","","","","","1B-2","Insights from HotChips 2025 Outstanding Papers","<b>Seunghyun Park, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
+DB[07] = new Array("","","","","","","","","1B-2","Insights from HotChips 2025 Outstanding Papers","<b>Seunghyun Park, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
 
-DB[05] = new Array("","","","","","","","","1B-3","RISC-V ISA Extension for Custom Acceleration","<b>Hoseong Kim, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
+DB[08] = new Array("","","","","","","","","1B-3","RISC-V ISA Extension for Custom Acceleration","<b>Hoseong Kim, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
 
-DB[06] = new Array("","","","","","","","","1B-4","TRU-Net Based AI Model Implementation by Pure C for Real-time Speech Enhancement","<b>Yonghun Lee, Daejin Park</b> (Gaonchips, Kyungpook National University, Korea)<br>","","","");
+DB[09] = new Array("","","","","","","","","1B-4","TRU-Net Based AI Model Implementation by Pure C for Real-time Speech Enhancement","<b>Yonghun Lee, Daejin Park</b> (Gaonchips, Kyungpook National University, Korea)<br>","","","");
 
   
 // Digital Twin and DSP
-DB[07] = new Array("1C","color-oral-06","1C","Aug. 28, 2025 (Friday)","12:00","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Digital Twin, Signal Processing","1C-1","FISTA-based Compressed Sensing - C Implementation","<b>Sungho Cho, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","1D");
+DB[10] = new Array("1C","color-oral-06","1C","Aug. 28, 2025 (Friday)","12:00","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Digital Twin, Signal Processing","1C-1","FISTA-based Compressed Sensing - C Implementation","<b>Sungho Cho, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","1D");
 
-DB[08] = new Array("","","","","","","","","1C-2","Protecting Magnetic Field Disturbance by Adding Steel Shield based on Magnetic Field Change Phenomenon","<b>Jaekyung Pi, Daejin Park</b> (Kyungchang Industry, Kyungpook National University, Korea)<br>","","","");
+DB[11] = new Array("","","","","","","","","1C-2","Protecting Magnetic Field Disturbance by Adding Steel Shield based on Magnetic Field Change Phenomenon","<b>Jaekyung Pi, Daejin Park</b> (Kyungchang Industry, Kyungpook National University, Korea)<br>","","","");
 
 // AI-Enabled Accelerated Embedded SW & Algorithm
-DB[09] = new Array("1D","color-oral-04","1D","Aug. 28, 2025 (Friday)","13:00","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","AI Accelerated Embedded Software","1D-1","Runtime Streamable Execution of Compressed On-Chip Software","<b>Minjung Kim, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","1E");
+DB[12] = new Array("1D","color-oral-04","1D","Aug. 28, 2025 (Friday)","13:00","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","AI Accelerated Embedded Software","1D-1","Runtime Streamable Execution of Compressed On-Chip Software","<b>Minjung Kim, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","1E");
 
-DB[10] = new Array("","","","","","","","","1D-2","DMA-Based BLDC Motor Control System Design Method for Efficient Guided Weapon Actuator Control","<b>Jongyun Byeon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
+DB[13] = new Array("","","","","","","","","1D-2","DMA-Based BLDC Motor Control System Design Method for Efficient Guided Weapon Actuator Control","<b>Jongyun Byeon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
 
-DB[11] = new Array("","","","","","","","","1D-3","On-Chip Binary Code Compression and Runtime Uncompressed SW Execution using Intel Hex Format Manipulation","<b>Hyunjung Lee, Daejin Park</b> (SL, Kyungpook National University, Korea)<br>","","","");
+DB[14] = new Array("","","","","","","","","1D-3","On-Chip Binary Code Compression and Runtime Uncompressed SW Execution using Intel Hex Format Manipulation","<b>Hyunjung Lee, Daejin Park</b> (SL, Kyungpook National University, Korea)<br>","","","");
 
-DB[12] = new Array("","","","","","","","","1D-4","Efficient CNN Inference with Feature Reuse and Selective Region Updates","<b>Yeonjae Lee, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
+DB[15] = new Array("","","","","","","","","1D-4","Efficient CNN Inference with Feature Reuse and Selective Region Updates","<b>Yeonjae Lee, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
 
-DB[13] = new Array("","","","","","","","","1D-5","Low-Power Autonomous Driving in Familiar Environments","<b>Mingyu Jung, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
+DB[16] = new Array("","","","","","","","","1D-5","Low-Power Autonomous Driving in Familiar Environments","<b>Mingyu Jung, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
 
 // Compiler Simulation, Software Framework
-DB[14] = new Array("1E","color-oral-05","1E","Aug. 28, 2025 (Friday)","14:00","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Compiler, Simulator, Software Framework", "1E-1", "LLVM-Based Efficient Hybrid Cache and TCM Memory Allocation for Low-Latency","<b>Gihyun Jeon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","1F");
+DB[17] = new Array("1E","color-oral-05","1E","Aug. 28, 2025 (Friday)","14:00","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Compiler, Simulator, Software Framework", "1E-1", "LLVM-Based Efficient Hybrid Cache and TCM Memory Allocation for Low-Latency","<b>Gihyun Jeon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","1F");
 
-DB[15] = new Array("","","","","","","","","1E-2","FOTA-based Updatable Software Runtime Architecture","<b>Janghun Lee, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
+DB[18] = new Array("","","","","","","","","1E-2","FOTA-based Updatable Software Runtime Architecture","<b>Janghun Lee, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
 
-DB[16] = new Array("","","","","","","","","1E-3","Real-time Control Performance during OTA Updates using Multi-core and DMA Priority Strategies","<b>Jaehyun Park, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
+DB[19] = new Array("","","","","","","","","1E-3","Real-time Control Performance during OTA Updates using Multi-core and DMA Priority Strategies","<b>Jaehyun Park, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
 
-DB[17] = new Array("","","","","","","","","1E-4","Containerization Strategy and Automated Fault Recovery through Dynamic Runtime Hot-Swapping in Naval Combat System Software","<b>Byeongin Jung, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
+DB[20] = new Array("","","","","","","","","1E-4","Containerization Strategy and Automated Fault Recovery through Dynamic Runtime Hot-Swapping in Naval Combat System Software","<b>Byeongin Jung, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
 
 
 
-DB[18] = new Array("","","","","","","","","1E-5","End of Symposium","","","","");
+DB[21] = new Array("","","","","","","","","1E-5","End of Symposium","","","","");
 
 
 
