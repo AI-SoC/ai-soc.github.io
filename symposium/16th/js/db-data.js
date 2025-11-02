941a36420f3623f8d9afbeadcdf04bcfb1787e90
AI-SoC <65325744+AI-SoC@users.noreply.github.com>
2025-11-02 23:59:02 +0900

    Add files via upload

--- symposium/16th/js/db-data.js
+++ symposium/16th/js/db-data.js
@@ -18,47 +18,47 @@ PS = new Array();
 
 // Cloud Metaverse
 
-DB[00] = new Array("1A","color-oral-01","1A","Oct. 31, 2025 (Friday)","10:00","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Large-Scaled System Integration","1B-1","Edge Robust Inference with Runtime Dynamic Reconfigurable FPGA","<b>Myeongjin Kang, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","1C");
+DB[00] = new Array("1A","color-oral-01","1A","Oct. 31, 2025 (Friday)","10:00","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Large-Scaled System Integration","1B-1","Edge Robust Inference with Runtime Dynamic Reconfigurable FPGA","<b>Myeongjin Kang, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/7ouEWvgSm5E","1C");
 
-DB[01] = new Array("","","","","","","","","1A-2","Asynchronous and Efficient General Matrix Multiplication Operations (GEMM) Implementation","<b>Sunghoon Hong, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
+DB[01] = new Array("","","","","","","","","1A-2","Asynchronous and Efficient General Matrix Multiplication Operations (GEMM) Implementation","<b>Sunghoon Hong, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/3p09bLJIyxs","");
 
-DB[02] = new Array("","","","","","","","","1A-3","Lightweight Statistical Imbalance Detection on Multi-Core TriCore MCU for Automotive BMS","<b>Gihyeon Seong, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
+DB[02] = new Array("","","","","","","","","1A-3","Lightweight Statistical Imbalance Detection on Multi-Core TriCore MCU for Automotive BMS","<b>Gihyeon Seong, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/63JkClE24fw","");
 
 
 // SoC
 DB[03] = new Array("1B","color-oral-02","1B","Oct. 31, 2025 (Friday)","11:00","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","System-on-Chip","1B-1","Do Execution Boundaries Belong in the Cache? Rethinking with RF-Scoped Commit","<b>Heejee Yun, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","1C");
 
-DB[04] = new Array("","","","","","","","","1B-2","The Statistics Behind Intelligence: Accelerating Inference through Model Analytics","<b>Seunghyun Park, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
+DB[04] = new Array("","","","","","","","","1B-2","The Statistics Behind Intelligence: Accelerating Inference through Model Analytics","<b>Seunghyun Park, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/bEuECGjdGiA","");
 
-DB[05] = new Array("","","","","","","","","1B-3","Consideration of Efficient Artificial Intelligence Processing :  Based on Systolic Tile Array and Memory Reshaping Allocation","<b>Hoseong Kim, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
+DB[05] = new Array("","","","","","","","","1B-3","Consideration of Efficient Artificial Intelligence Processing :  Based on Systolic Tile Array and Memory Reshaping Allocation","<b>Hoseong Kim, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/cYDvzED4844","");
 
-DB[06] = new Array("","","","","","","","","1B-4","C Modeling and Accelerator Design for Floating-Point Unit","<b>Yonghun Lee, Daejin Park</b> (Gaonchips, Kyungpook National University, Korea)<br>","","","");
+DB[06] = new Array("","","","","","","","","1B-4","C Modeling and Accelerator Design for Floating-Point Unit","<b>Yonghun Lee, Daejin Park</b> (Gaonchips, Kyungpook National University, Korea)<br>","","https://youtu.be/zpZbDzZ17dc","");
 
   
 // Digital Twin and DSP
-DB[07] = new Array("1C","color-oral-06","1C","Oct. 31, 2025 (Friday)","12:00","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Digital Twin, Signal Processing","1C-1","Resource efficient Multi-tone Touch System","<b>Sungho Cho, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","1D");
+DB[07] = new Array("1C","color-oral-06","1C","Oct. 31, 2025 (Friday)","12:00","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Digital Twin, Signal Processing","1C-1","Resource efficient Multi-tone Touch System","<b>Sungho Cho, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/3NxZxDxabLU","1D");
 
-DB[08] = new Array("","","","","","","","","1C-2","Magnetic Permeability Measurement","<b>Jaekyung Pi, Daejin Park</b> (Kyungchang Industry, Kyungpook National University, Korea)<br>","","","");
+DB[08] = new Array("","","","","","","","","1C-2","Magnetic Permeability Measurement","<b>Jaekyung Pi, Daejin Park</b> (Kyungchang Industry, Kyungpook National University, Korea)<br>","","https://youtu.be/FZzgvqjZuyo","");
 
 // AI-Enabled Accelerated Embedded SW & Algorithm
-DB[09] = new Array("1D","color-oral-04","1D","Oct. 31, 2025 (Friday)","13:00","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","AI Accelerated Embedded Software","1D-1","Adaptive Scheduling Strategies for Multi-Core RTOS : Blending Preemptive and Nonpreemptive Approaches","<b>Minjung Kim, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","1E");
+DB[09] = new Array("1D","color-oral-04","1D","Oct. 31, 2025 (Friday)","13:00","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","AI Accelerated Embedded Software","1D-1","Adaptive Scheduling Strategies for Multi-Core RTOS : Blending Preemptive and Nonpreemptive Approaches","<b>Minjung Kim, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/wyygmDXl7qg","1E");
 
-DB[10] = new Array("","","","","","","","","1D-2","A Study on the DMA-based BLDC Motor Control System Using Variable Buffers Applicable to Guided Weapon Driving Devices","<b>Jongyun Byeon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
+DB[10] = new Array("","","","","","","","","1D-2","A Study on the DMA-based BLDC Motor Control System Using Variable Buffers Applicable to Guided Weapon Driving Devices","<b>Jongyun Byeon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/20KVSlyZeHk","");
 
-DB[11] = new Array("","","","","","","","","1D-3","A Deep Learning Approach for High-Ratio Binary Compression of Control Algorithms on the TriCore Architecture","<b>Hyunjung Lee, Daejin Park</b> (SL, Kyungpook National University, Korea)<br>","","","");
+DB[11] = new Array("","","","","","","","","1D-3","A Deep Learning Approach for High-Ratio Binary Compression of Control Algorithms on the TriCore Architecture","<b>Hyunjung Lee, Daejin Park</b> (SL, Kyungpook National University, Korea)<br>","","https://youtu.be/L3vJ5aUJ8rk","");
 
-DB[12] = new Array("","","","","","","","","1D-4","Smooth Obstacle Avoidance Path Planning for Autonomous Vehicles","<b>Yeonjae Lee, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
+DB[12] = new Array("","","","","","","","","1D-4","Smooth Obstacle Avoidance Path Planning for Autonomous Vehicles","<b>Yeonjae Lee, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/y4HBt5PHjV4","");
 
-DB[13] = new Array("","","","","","","","","1D-5","Adaptive Forward Perception Based on Scene Familiarity in Autonomous Driving","<b>Mingyu Jung, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
+DB[13] = new Array("","","","","","","","","1D-5","Adaptive Forward Perception Based on Scene Familiarity in Autonomous Driving","<b>Mingyu Jung, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/2c1qLtayYM0","");
 
 // Compiler Simulation, Software Framework
-DB[14] = new Array("1E","color-oral-05","1E","Oct. 31, 2025 (Friday)","14:00","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Compiler, Simulator, Software Framework", "1E-1", "Efficient Memory Allocation Considering WCET and Execution time","<b>Gihyun Jeon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","1F");
+DB[14] = new Array("1E","color-oral-05","1E","Oct. 31, 2025 (Friday)","14:00","Room A","Chair: Daejin Park (Kyungpook National University, Korea)","Compiler, Simulator, Software Framework", "1E-1", "Efficient Memory Allocation Considering WCET and Execution time","<b>Gihyun Jeon, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/e7aS4Ei4_qE","1F");
 
-DB[15] = new Array("","","","","","","","","1E-2","Encrypted Segment Construction for Decentralized Runtime Execution over Asymmetric Vehicle Networks","<b>Janghun Lee, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
+DB[15] = new Array("","","","","","","","","1E-2","Encrypted Segment Construction for Decentralized Runtime Execution over Asymmetric Vehicle Networks","<b>Janghun Lee, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/wfXaJItPjVE","");
 
-DB[16] = new Array("","","","","","","","","1E-3","Real-time Control Performance during OTA Updates using Multi-core and DMA Priority Strategies","<b>Jaehyun Park, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
+DB[16] = new Array("","","","","","","","","1E-3","Real-time Control Performance during OTA Updates using Multi-core and DMA Priority Strategies","<b>Jaehyun Park, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/nhCRJHBI6C4","");
 
-DB[17] = new Array("","","","","","","","","1E-4","Mission-Aware Runtime Hot-Swap for High-Availability Containerized Naval CMS Software Architecture","<b>Byeongin Jung, Daejin Park</b> (Kyungpook National University, Korea)<br>","","","");
+DB[17] = new Array("","","","","","","","","1E-4","Mission-Aware Runtime Hot-Swap for High-Availability Containerized Naval CMS Software Architecture","<b>Byeongin Jung, Daejin Park</b> (Kyungpook National University, Korea)<br>","","https://youtu.be/TxLtOjvQLGc","");
 
 
 
